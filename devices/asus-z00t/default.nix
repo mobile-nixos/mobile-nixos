982a705dc272e548f32af5f8422858187322c353
Samuel Dionne-Riel <samuel@dionne-riel.com>
2018-06-17 18:53:20 +0000

    Removes dead code.

--- devices/asus-z00t/default.nix
+++ devices/asus-z00t/default.nix
@@ -1,13 +1,10 @@
 { config, lib, pkgs, ... }:
-let
-  msm-fb-refresher = (import ../../quirks/qualcomm/msm-fb-refresher.nix) { inherit pkgs lib; };
-in
+
 {
   mobile.device.name = "asus-z00t";
   mobile.device.info = (lib.importJSON ../postmarketOS-devices.json).asus-z00t // {
     # TODO : make kernel part of options.
     kernel = pkgs.callPackage ./kernel { kernelPatches = pkgs.defaultKernelPatches; };
-    # TODO : make stage-1 part of options.
   };
   mobile.hardware = {
     # This could also be pre-built option types?
