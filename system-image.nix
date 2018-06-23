99640cb723b47f02c03d0d1a423487f6b9adabe1
Samuel Dionne-Riel <samuel@dionne-riel.com>
2018-06-23 18:47:13 -0400

    WIP : makes system-image an installation device.
    This gives us passwordless root.
    
    This may be the final form of the system image.

--- system-image.nix
+++ system-image.nix
@@ -17,6 +17,8 @@ in
     # infinite recursion encountered, at /nix/store/8vczq3489dl8xa5s7ksqyqkbirmpd3sb-source/lib/modules.nix:163:28
     # (pkgs.path + "/nixos/modules/installer/cd-dvd/sd-image.nix")
     (path + "/nixos/modules/installer/cd-dvd/sd-image.nix")
+    (path + "/nixos/modules/profiles/base.nix")
+    (path + "/nixos/modules/profiles/installation-device.nix")
   ];
   boot.loader.grub.enable = false;
   boot.loader.generic-extlinux-compatible.enable = true;
