be0d3b6fb8f76b3419ee9cfd38530911ebcac245
Samuel Dionne-Riel <samuel@dionne-riel.com>
2018-06-27 20:06:36 -0400

    Revert "WIP: fbv builds, but seems not to work."
    This reverts commit 0df9018ab961d557f41667d6c3600f31652b0c12.

--- overlay/fbv/default.nix
+++ overlay/fbv/default.nix
@@ -7,33 +7,12 @@ stdenv.mkDerivation {
   inherit version;
   name = "fbv";
 
-  # Hmm, their library detection seems screwy.
-  # This is why I need to repeat both sets.
   buildInputs = [
     libpng
     libjpeg
     libungif
     getopt
   ];
-
-  nativeBuildInputs = [
-    libpng
-    libjpeg
-    libungif
-    getopt
-  ];
-
-  preConfigure = ''
-    # Using --libs= doesn't work.
-    export LDFLAGS="-lpng -lz -ljpeg -lungif"
-  '';
-
-  postPatch = ''
-    substituteInPlace Makefile --replace "CC${"\t"}= gcc " "CC = $CC";
-    substituteInPlace Makefile --replace '$(CC) $(LDFLAGS)' '$(LD) $(LDFLAGS)'
-  '';
-
-  configurePlatforms = [ ];
 
   src = fetchurl {
     url = "http://s-tech.elsat.net.pl/fbv/fbv-${version}.tar.gz";
