a5b9e275ed6628d44589a62763d3a113c6848d91
Samuel Dionne-Riel <samuel@dionne-riel.com>
2018-07-01 17:42:33 -0400

    Adds `asus-flo` with lineageos kernel
    Current issues:
    
     * Framebuffer colors are wrong. (RGBA → BGRA)

---
+++ devices/asus-flo/kernel/default.nix
@@ -0,0 +1,118 @@
+{ stdenv, hostPlatform
+, overrideCC
+, gcc6
+, fetchurl
+, fetchFromGitHub
+, linuxManualConfig
+, firmwareLinuxNonfree
+, bison, flex
+, binutils-unwrapped
+, dtbTool
+, kernelPatches ? []
+, buildPackages
+}:
+
+# Inspired by https://github.com/thefloweringash/rock64-nix/blob/master/packages/linux_ayufan_4_4.nix
+# Then in turn inspired by the postmarketos APKBUILDs.
+
+let
+  modDirVersion = "3.4.113";
+
+  version = "${modDirVersion}";
+  src = fetchFromGitHub {
+    owner = "LineageOS";
+    repo = "android_kernel_google_msm";
+    rev = "a4b9cf707b9acf6e5f6089d1121ae973efe399b0";
+    sha256 = "0q88sqmcd09m0wq27rvzvq588gbk3daji1zp36qpyzl1d66b37v6";
+  };
+  patches = [
+    ./00_fix_return_address.patch
+    ./02_gpu-msm-fix-gcc5-compile.patch
+    ./03-fix-video-argb-setting.patch
+    ./patch_fsp_detect.patch
+    ./patch_lifebook_detect.patch
+    ./90_dtbs-install.patch
+  ];
+  postPatch = ''
+    patchShebangs .
+
+    cp -v "${./compiler-gcc6.h}" "./include/linux/compiler-gcc6.h"
+
+    # Remove -Werror from all makefiles
+    local i
+    local makefiles="$(find . -type f -name Makefile)
+    $(find . -type f -name Kbuild)"
+    for i in $makefiles; do
+      sed -i 's/-Werror-/-W/g' "$i"
+      sed -i 's/-Werror//g' "$i"
+    done
+    echo "Patched out -Werror"
+  '';
+
+  additionalInstall = ''
+    mkdir -p "$out/boot"
+
+    # Copies all potential output files.
+    for f in zImage-dtb Image.gz-dtb zImage Image.gz Image; do
+      f=arch/arm/boot/$f
+      [ -e "$f" ] || continue
+      echo "zImage found: $f"
+      cp -v "$f" "$out/"
+      break
+    done
+
+    cp -v "arch/arm/boot/zImage" "$out/vmlinuz"
+
+    # Copies the dtb, could always be useful.
+    mkdir -p $out/dtb
+    for f in arch/*/boot/dts/*.dtb; do
+      cp -v "$f" $out/dtb/
+    done
+
+    # Copies the .config file to output.
+    # Helps ensuring sanity.
+    cp -v .config $out/src.config
+  '';
+in
+let
+  buildLinux = (args: (linuxManualConfig args).overrideAttrs ({ makeFlags, postInstall, ... }: {
+    inherit patches postPatch;
+    postInstall = ''
+      ${postInstall}
+
+      ${additionalInstall}
+    '';
+    installTargets = [ "zinstall" ];
+    dontStrip = true;
+  }));
+
+  configfile = stdenv.mkDerivation {
+    name = "android-asus-flo-config-${modDirVersion}";
+    inherit version;
+    inherit src patches postPatch;
+    nativeBuildInputs = [bison flex];
+
+    buildPhase = ''
+      echo "building config file"
+      cp -v ${./config-asus-flo.armv7} .config
+      yes "" | make $makeFlags "''${makeFlagsArray[@]}" oldconfig || :
+    '';
+
+    installPhase = ''
+      cp -v .config $out
+    '';
+  };
+
+in
+
+buildLinux {
+  inherit kernelPatches;
+  inherit hostPlatform;
+  inherit src;
+  inherit version;
+  inherit modDirVersion;
+  inherit configfile;
+  stdenv = overrideCC stdenv buildPackages.gcc6;
+
+  allowImportFromDerivation = true;
+}
