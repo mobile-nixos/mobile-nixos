0acd0f54e24912cd9cbb42cf2dd801d87221ae4f
Samuel Dionne-Riel <samuel@dionne-riel.com>
2018-07-12 14:41:02 -0400

    Removes unneeded patches

--- overlay/overlay.nix
+++ overlay/overlay.nix
@@ -50,9 +50,6 @@ in
     defaultKernelPatches = with self.kernelPatches; [
       bridge_stp_helper
       p9_fixes
-      lguest_entry-linkage
-      packet_fix_race_condition_CVE_2016_8655
-      DCCP_double_free_vulnerability_CVE-2017-6074
     ];
 
     #
