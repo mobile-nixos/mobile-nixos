ac2ad419f7801a94273399533703dc78e10f5a71
Samuel Dionne-Riel <samuel@dionne-riel.com>
2019-04-19 22:56:36 -0400

    kernel-builder: Adds a WIP Pre-init logo
    When using CONFIG_LOGO, this "pre-init" logo will be shown in the
    center. This abuses the Linux mascot logo feature to help end-users
    figure out without console logging whether the kernel is hung really
    early, or after the framebuffer kicked-in. The initrd is expected to
    replace the logo ASAP.
    
    This is to be changed for something better integrated into the style
    guide once written up.

--- overlay/mobile-nixos/kernel/builder.nix
+++ overlay/mobile-nixos/kernel/builder.nix
@@ -130,6 +130,26 @@ stdenv.mkDerivation {
         --replace NIXOS_RANDSTRUCT_SEED \
         $(echo ${src} ${configfile} | sha256sum | cut -d ' ' -f 1 | tr -d '\n')
     fi
+
+    # FIXME : make optional...
+    # Makes the "logo" option show only one logo and not dependent on cores.
+    # This should be "safer" than a patch on a greater range of kernel versions.
+    # Also defaults to centering when possible.
+    
+    if [ -e drivers/video/fbdev/core/fbmem.c ]; then
+      sed -i -e 's/num_online_cpus()/1/g' \
+        drivers/video/fbdev/core/fbmem.c
+      sed -i -e '/^bool fb_center_logo/ s/;/ = true;/' \
+        drivers/video/fbdev/core/fbmem.c
+    fi
+    if [ -e drivers/video/fbmem.c ]; then
+      sed -i -e 's/num_online_cpus()/1/g' \
+        drivers/video/fbmem.c
+    fi
+
+    # Overrides the kernel logo
+    cp ${./logo_linux_clut224.ppm} drivers/video/logo/logo_linux_clut224.ppm
+
     ${postPatch}
   '';
 
