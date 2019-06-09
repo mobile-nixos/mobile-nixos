e88ba541e740ff801cdc03e9078817f91a30a10a
Samuel Dionne-Riel <samuel@dionne-riel.com>
2019-06-09 14:34:07 -0400

    APQ8064–1AA: Use `msm-fb-refresher`

--- modules/hardware-qualcomm.nix
+++ modules/hardware-qualcomm.nix
@@ -40,6 +40,7 @@ in
     {
       mobile = mkIf cfg.qualcomm-apq8064-1aa.enable {
         system.platform = "armv7a-linux";
+        quirks.qualcomm.msm-fb-refresher.enable = true;
       };
     }
   ];
