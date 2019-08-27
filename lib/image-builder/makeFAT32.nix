549f0b55eb239f3d88805adeca0727880598fc7f
Samuel Dionne-Riel <samuel@dionne-riel.com>
2019-08-27 14:14:57 -0400

    WIP adds makeFAT32

---
+++ lib/image-builder/makeFAT32.nix
@@ -0,0 +1,34 @@
+{ makePartition, dosfstools, mtools, libfaketime }:
+
+/*  */ let scope = { "fileSystem.makeFAT32" =
+
+{ partitionID, ... } @ args:
+makePartition (args // {
+  # FAT32 can be used for ESP. Let's make this obvious.
+  filesystemType = if args ? filesystemType then args.filesystemType else "FAT32";
+
+  nativeBuildInputs = [
+    libfaketime
+    dosfstools
+    mtools
+  ];
+
+  filesystemPhase = ''
+    faketime "1970-01-01 00:00:00" mkfs.vfat \
+      -i $partitionID \
+      -n $partName \
+      "$img"
+  '';
+
+  copyPhase = ''
+    # FIXME : here mcopy files
+    echo ... would copy here ...
+  '';
+
+  checkPhase = ''
+    # Always verify FS
+    fsck.vfat -vn "$img"
+  '';
+})
+
+/*  */ ;}; in scope."fileSystem.makeFAT32"
