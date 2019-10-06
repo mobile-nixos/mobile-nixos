f586a4197d2a8d3712ab78e0de11f5be5eb59cea
adisbladis <adisbladis@gmail.com>
2019-10-06 22:29:45 +0100

    libhybris: Enable xorg/wayland support & apply PMos patches

--- overlay/libhybris/default.nix
+++ overlay/libhybris/default.nix
@@ -2,15 +2,26 @@
 stdenv
 , lib
 , fetchFromGitHub
+, fetchpatch
 , autoreconfHook
 , pkgconfig
 
 , android-headers
 , file
+, wayland
+, xorg
 }:
 
 let
   inherit (stdenv) targetPlatform;
+
+  fetchpmospatch = {name, sha256}: fetchpatch {
+    inherit name;
+    url = "https://gitlab.com/postmarketOS/pmaports/raw/c2cb2a17d47458083f4e62714a46b1875d73cebb/hybris/libhybris/${name}";
+    inherit sha256;
+    stripLen = 1;
+  };
+
 in
 stdenv.mkDerivation {
   name = "libhybris";
@@ -19,9 +30,29 @@ stdenv.mkDerivation {
   src = fetchFromGitHub {
     owner = "libhybris";
     repo = "libhybris";
-    rev = "07b547e90db625685050bdfd00c92ccafc64aa09";
-    sha256 = "0g0cqjydbahbzas40vz0awwyw2xjyjj3hxrkdydkn9qscyiyx593";
+    # Use same rev as postmarketOS so patches applies
+    rev = "8ddb15b53d6a63b1545bbf97d00ea93827bd68eb";
+    sha256 = "1xv714f8xz9j83y4snchz0m6flgvf82nhr75hdjwcjn3rlkwq2vb";
   };
+
+  patches = [
+    (fetchpmospatch {
+      name = "0002-tests-Regression-test-for-EGL-glibc-TLS-conflict.patch";
+      sha256 = "0nzw51hd8z1xz7vjlr1hsban4aikapq71g3r2wsil0jjk745b24z";
+    })
+    (fetchpmospatch {
+      name = "0003-PATCH-v2-Implement-X11-EGL-platform-based-on-wayland.patch";
+      sha256 = "0ijfygxw6hlgam74cjbygr6agy60b24nwjg14q1a0xnrbs5g828x";
+    })
+    (fetchpmospatch {
+      name = "0004-Build-test-hwcomposer-7-caf.patch";
+      sha256 = "0zdphi1ypw3l2z10nrr60xx2ymcnff1h03sc228q57kl46cd9nhm";
+    })
+    (fetchpmospatch {
+      name = "0005-eglplatform_wayland-link-libEGL-at-runtime.patch";
+      sha256 = "1fgydb2i6awska47ajylahfw7azndqqc31nazvicai10b47b151b";
+    })
+  ];
 
   postAutoreconf = ''
     substituteInPlace configure \
@@ -29,6 +60,9 @@ stdenv.mkDerivation {
   '';
 
   configureFlags = [
+    "--enable-wayland"
+    "--enable-trace"
+    "--enable-experimental"
     "--with-android-headers=${android-headers}/include/android/"
   ]
   ++ lib.optional targetPlatform.isAarch64 "--enable-arch=arm64"
@@ -40,5 +74,11 @@ stdenv.mkDerivation {
   nativeBuildInputs = [
     autoreconfHook
     pkgconfig
+  ];
+
+  buildInputs = [
+    wayland
+    xorg.libX11
+    xorg.libXext
   ];
 }
