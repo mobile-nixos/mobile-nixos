0a49490cf05d6ab9db32cc48d48cfa56ae9b4fca
Samuel Dionne-Riel <samuel@dionne-riel.com>
2019-10-07 20:00:07 -0400

    examples/demo: Make onboard bigger by default

--- examples/demo/configuration.nix
+++ examples/demo/configuration.nix
@@ -189,6 +189,26 @@ in
       environment.systemPackages = with pkgs; [
         onboard
       ];
+      environment.etc."xdg/autostart/onboard-boottime-configuration.desktop" = {
+        text = let script = pkgs.writeShellScript "onboard-boottime-configuration" ''
+          set -u
+          set -e
+
+          # A bit rude, but this ensures the keyboard always starts at a quarter
+          # of the resolution.
+          # onboard will not accept -s to set size with a docked keyboard.
+          height=$(( $( ${pkgs.xlibs.xwininfo}/bin/xwininfo -root | grep '^\s\+Height:' | cut -d':' -f2 ) / 4 ))
+
+          ${pkgs.gnome3.dconf}/bin/dconf write /org/onboard/window/landscape/dock-height "$height" || :
+          ${pkgs.gnome3.dconf}/bin/dconf write /org/onboard/window/portrait/dock-height "$height"  || :
+        '';
+        in ''
+          [Desktop Entry]
+          Name=Onboard boot time configuration
+          Exec=${script}
+          X-XFCE-Autostart-Override=true
+        '';
+      };
       environment.etc."xdg/autostart/onboard-autostart.desktop" = {
         source = pkgs.runCommandNoCC "onboard-autostart.desktop" {} ''
           cat "${pkgs.onboard}/etc/xdg/autostart/onboard-autostart.desktop" > $out
