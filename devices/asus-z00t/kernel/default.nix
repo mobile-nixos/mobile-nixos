ff5de40077bd59a1584fad829714e98df283e833
Samuel Dionne-Riel <samuel@dionne-riel.com>
2019-10-09 18:49:47 -0400

    asus-z00t: update kernel to latest lineageos revision

--- devices/asus-z00t/kernel/default.nix
+++ devices/asus-z00t/kernel/default.nix
@@ -15,8 +15,8 @@
   src = fetchFromGitHub {
     owner = "LineageOS";
     repo = "android_kernel_asus_msm8916";
-    rev = "d56000991e7d90e3a75afd86fb2f3c779232ff29"; # lineage-15.1
-    sha256 = "079sm5z0ml0ijm866ga5mzwnix4wzvida0469vymbrh8mhz47p4r";
+    rev = "1a45c63742b8c3253a38c2ff97b672918c88d8df"; # lineage-15.1
+    sha256 = "02mfz3h5s3lvkdinglqmhm2hyfw4w0hqzzh1xla1i9wfc31ddbap";
   };
 
   patches = [
