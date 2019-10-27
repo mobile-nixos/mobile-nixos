79ac51e5844664f1e8259795f6e94dfba52ea04b
Craige McWhirter <craige@mcwhirter.io>
2019-10-28 00:22:25 +1000

    Corrected bash syntax

--- systems/bootimg.nix
+++ systems/bootimg.nix
@@ -26,31 +26,31 @@ pkgs.stdenv.mkDerivation {
   ];
 
   installPhase = ''
-	echo Using kernel: ${kernel_file}
-(
-PS4=" $ "
-set -x
-    mkdir -pv $out/nix-support
-    mkbootimg \
-      --kernel  ${kernel_file} \
-      ${
-        if with_qcdt then
-          "--dt ${dtb}"
-        else
+    echo Using kernel: ${kernel_file}
+    (
+      PS4=" $ "
+      set -x
+      mkdir -pv $out/nix-support
+      mkbootimg \
+        --kernel  ${kernel_file} \
+        ${
+          if with_qcdt then
+            "--dt ${dtb}"
+          else
           ""
-      } \
-      --ramdisk ${initrd} \
-      --cmdline       "${cmdline}" \
-      --base           ${device_info.flash_offset_base   } \
-      --kernel_offset  ${device_info.flash_offset_kernel } \
-      --second_offset  ${device_info.flash_offset_second } \
-      --ramdisk_offset ${device_info.flash_offset_ramdisk} \
-      --tags_offset    ${device_info.flash_offset_tags   } \
-      --pagesize       ${device_info.flash_pagesize      } \
-      -o $out/nix-support/${device_name}_boot.img
-      cat <<EOF > $out/nix-support/hydra-build-products
-      file bootimage $out/nix-support/${device_name}_boot.imgo
-      EOF
-)
+        } \
+        --ramdisk ${initrd} \
+        --cmdline       "${cmdline}" \
+        --base           ${device_info.flash_offset_base   } \
+        --kernel_offset  ${device_info.flash_offset_kernel } \
+        --second_offset  ${device_info.flash_offset_second } \
+        --ramdisk_offset ${device_info.flash_offset_ramdisk} \
+        --tags_offset    ${device_info.flash_offset_tags   } \
+        --pagesize       ${device_info.flash_pagesize      } \
+        -o $out/nix-support/${device_name}_boot.img
+        cat <<EOF > $out/nix-support/hydra-build-products
+    file bootimage $out/nix-support/${device_name}_boot.img
+    EOF
+    )
   '';
 }
