7103b2cd26100e46bb43cd85f4358144a86d06a1
Craige McWhirter <craige@mcwhirter.io>
2019-10-27 22:43:27 +1000

    Enabled CMDLINE_DROP_DANGEROUS_ANDROID_OPTIONS
    Ignores dangerous Android options

--- devices/google-blueline/kernel/default.nix
+++ devices/google-blueline/kernel/default.nix
@@ -27,7 +27,6 @@ in
     ./0001-mobile-nixos-Adds-and-sets-BGRA-as-default.patch
     ./0001-mobile-nixos-Workaround-selected-processor-does-not-.patch
     ./0003-arch-arm64-Add-config-option-to-fix-bootloader-cmdli.patch
-    #./117ffa9fe6994b82604c71f93332537e362f3cb7.patch
   ];
 
   isModular = false;
