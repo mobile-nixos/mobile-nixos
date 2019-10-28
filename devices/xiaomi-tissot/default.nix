25ab9e3247b2feec2881b409fbd82129a0b58a77
Linus Heckemann <git@sphalerite.org>
2019-10-28 15:51:29 +0100

    devices/xiaomi-tissot: init
    Kernel patch is copied from xiaomi-lavender.

---
+++ devices/xiaomi-tissot/default.nix
@@ -0,0 +1,29 @@
+{ config, lib, pkgs, ... }:
+let
+  inherit (config.mobile.device) name;
+in {
+  mobile.device.name = "xiaomi-tissot";
+  mobile.device.info = {
+    name = "Xiaomi A1";
+    screen_width = 1080;
+    screen_height = 1920;
+    kernel_cmdline = "androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78af000 androidboot.selinux=permissive buildvariant=eng";
+    flash_offset_base = "0x80000000";
+    flash_offset_kernel = "0x00008000";
+    flash_offset_second = "0x00f00000";
+    flash_offset_ramdisk = "0x01000000";
+    flash_offset_tags = "0x00000100";
+    flash_pagesize = "2048";
+    kernel = pkgs.callPackage ./kernel { kernelPatches = pkgs.defaultKernelPatches; };
+    #dtb = "${kernel}/dtbs/msm8953-qrd-sku3-tissot.dtb";
+  };
+  mobile.hardware = {
+    soc = "qualcomm-msm8953";
+    ram = 1024 * 4;
+    screen = {
+      width = 1080; height = 1920;
+    };
+  };
+
+  mobile.system.type = "android";
+}
---
+++ devices/xiaomi-tissot/kernel/default.nix
@@ -0,0 +1,43 @@
+{ mobile-nixos
+, fetchFromGitHub
+, kernelPatches ? []
+, dtbTool
+, buildPackages
+}:
+let inherit (buildPackages) dtc; in
+(mobile-nixos.kernel-builder-gcc6 {
+  version = "3.18.71";
+  configfile = ./config.aarch64;
+  file = "Image.gz-dtb";
+  hasDTB = true;
+  src = fetchFromGitHub {
+    owner = "lineageos";
+    repo = "android_kernel_xiaomi_msm8953";
+    rev = "80cb3f607eb78280642c3b9b6e89f676e9c263bf";
+    sha256 = "13p326acpyqvlh5524bvy2qkgzgyhwxgy0smlwmcdl6y7yi04rg5";
+  };
+  patches = [
+    ./0003-arch-arm64-Add-config-option-to-fix-bootloader-cmdli.patch
+  ];
+
+  postPatch = ''
+    # Remove -Werror from all makefiles
+    local i
+    local makefiles="$(find . -type f -name Makefile)
+    $(find . -type f -name Kbuild)"
+    for i in $makefiles; do
+      sed -i 's/-Werror-/-W/g' "$i"
+      sed -i 's/-Werror//g' "$i"
+    done
+    echo "Patched out -Werror"
+  '';
+
+  makeFlags = [ "DTC_EXT=${dtc}/bin/dtc" ];
+
+  isModular = false;
+}).overrideAttrs ({ postInstall ? "", postPatch ? "", ... }: {
+  installTargets = [ "Image.gz" "zinstall" "Image.gz-dtb" "install" ];
+  postInstall = postInstall + ''
+    cp $buildRoot/arch/arm64/boot/Image.gz-dtb $out/
+  '';
+})
