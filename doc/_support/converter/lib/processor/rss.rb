810e18a75874c9a3748d13e066d20add51f1304f
Samuel Dionne-Riel <samuel@dionne-riel.com>
2019-12-02 17:18:47 -0500

    doc: Add RSS generation

---
+++ doc/_support/converter/lib/processor/rss.rb
@@ -0,0 +1,54 @@
+require "date"
+
+module Processor
+  # TODO: common base class or mixin with Sitemap
+  class RSS
+    Path = /^news\//
+    include ERB::Util
+
+    def to_rss(entries)
+      entry_template = ERB.new(File.read(File.join($options["root"], "_support/rss_entry.xml.erb")))
+      item_nodes = entries.map do |entry|
+        entry_template.result(binding)
+      end
+
+      document_template = ERB.new(File.read(File.join($options["root"], "_support/rss.xml.erb")))
+      document_template.result(binding)
+    end
+
+    attr_reader :sitemap
+
+    def strip_unneecessary(contents)
+      contents
+        .gsub(/<header.*<\/header>/m, "")
+        .gsub(/<footer.*<\/footer>/m, "")
+    end
+
+    def full_url(path)
+      [
+        "https://mobile.nixos.org",
+        path
+      ].join("/")
+    end
+
+    def initialize(sitemap)
+    end
+
+    def generate()
+      puts "[S] Generating sitemap..."
+      entries = Article.get_article_paths().map do |filename|
+        entry = Article.new(filename)
+        {
+          title: entry.title,
+          url: full_url(entry.out_path),
+          date: entry.date,
+          contents: strip_unneecessary(entry.output),
+        }
+      end
+
+      File.open(File.join($options["output_dir"], "index.xml"), "w") do |file|
+        file.write(to_rss(entries))
+      end
+    end
+  end
+end
