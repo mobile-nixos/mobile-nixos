6f25c1ee3b9b1c3496b17c3fe88a31b900a8a956
Samuel Dionne-Riel <samuel@dionne-riel.com>
2019-12-21 22:23:25 -0500

    boot/init: Import ruby shellwords

--- boot/init/default.nix
+++ boot/init/default.nix
@@ -1,10 +1,22 @@
-{ mruby, mrbgems }:
+{ fetchurl, mruby, mrbgems }:
 
+let
+  ruby_rev = "37457117c941b700b150d76879318c429599d83f";
+  shellwords = fetchurl {
+    name = "shellwords.rb";
+    url = "https://raw.githubusercontent.com/ruby/ruby/${ruby_rev}/lib/shellwords.rb";
+    sha256 = "197g7qvrrijmajixa2h9c4jw26l36y8ig6qjb5d43qg4qykhqfcx";
+  };
+in
 mruby.builder {
   pname = "mobile-nixos-init";
   version = "0.0-unstable";
 
   src = ./.;
+
+  postPatch = ''
+    cp ${shellwords} lib/0001_shellwords.rb
+  '';
 
   buildPhase = ''
     makeBin init main.rb
