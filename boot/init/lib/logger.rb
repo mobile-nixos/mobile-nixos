8b4ae5b2fcf7abaec1b8d696ff90518b17310298
Samuel Dionne-Riel <samuel@dionne-riel.com>
2019-12-23 14:46:16 -0500

    boot/init: Logger is configurable

--- boot/init/lib/logger.rb
+++ boot/init/lib/logger.rb
@@ -1,6 +1,10 @@
-# FIXME: Allow overriding $logger.level = Logger::DEBUG at build-time
-#LOG_LEVEL = Logger::INFO
-LOG_LEVEL = Logger::DEBUG
+LOG_LEVEL = Logger::INFO
+if Configuration["log"]
+  level = Configuration["log"]["level"]
+  if level
+    LOG_LEVEL = Logger.const_get(level.to_sym)
+  end
+end
 $logger = Logger.new(STDOUT, level: LOG_LEVEL)
 $logger.debug("Logger initialized... (#{LOG_LEVEL})")
 
