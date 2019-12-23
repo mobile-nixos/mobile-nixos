293d949648c3864d4efc5c304a642e8272ab7c81
Samuel Dionne-Riel <samuel@dionne-riel.com>
2019-12-23 13:58:35 -0500

    boot/init: Print mkdir_p for debug

--- boot/init/lib/monkey_patches/fileutils.rb
+++ boot/init/lib/monkey_patches/fileutils.rb
@@ -1,6 +1,7 @@
 class FileUtils
   def self.mkdir_p(*dirs)
     dirs.each do |dir|
+      $logger.debug(" $ mkdir -p #{dir}")
       # Create all paths in the given hierarchy
       dir.split("/").reduce("") do |component, acc|
         dir = [component, acc].join("/")
