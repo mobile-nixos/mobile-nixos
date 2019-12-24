962b36ec97bc7437c185c3e9ab0904482ee0d066
Samuel Dionne-Riel <samuel@dionne-riel.com>
2019-12-24 00:21:41 -0500

    boot/init: Implements Target tasks

---
+++ boot/init/tasks/target.rb
@@ -0,0 +1,46 @@
+# Use this task to define a "well-known" task that can be depended upon to stop
+# execution until multiple tasks have been fulfilled.
+#
+# An example is to make a Target that depends on all mount points needed for
+# booting the system, and the task that boots depend on that target.
+#
+# This removes the need of the final tasks depending on the target to need to
+# know about the dependencies.
+#
+# Prefer actually depending on discrete tasks rather than targets. Use targets
+# only to describe a vague step that may be fulfilled by different
+# implementations.
+class Tasks::Target < Task
+  def initialize(name)
+    @name = name.to_sym
+  end
+
+  def run(); end
+
+  def name()
+    "#{super}<#{@name}>"
+  end
+end
+
+module Targets
+  def self.[](name)
+    name = name.to_sym
+    $logger.debug("Asking for target #{name}")
+    @targets ||= {}
+    @targets[name] = Tasks::Target.new(name) unless @targets[name]
+    @targets[name]
+  end
+end
+
+module Dependencies
+  class Target < BaseDependency
+    def initialize(name)
+      @name = name.to_sym
+    end
+
+    def fulfilled?()
+      task = Targets[@name]
+      task && task.ran
+    end
+  end
+end
