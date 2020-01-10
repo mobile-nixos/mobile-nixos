5fa2110c7b724a05c2994d0d0660ff8246ef4fff
Samuel Dionne-Riel <samuel@dionne-riel.com>
2020-01-10 00:45:39 -0500

    hard-{shutdown,reboot}: remove
    This will be replaced with a better utility.

--- modules/initrd-base.nix
+++ modules/initrd-base.nix
@@ -6,20 +6,8 @@ let
   cfg = config.mobile.boot.stage-1;
 in
 {
-  options.mobile.boot.stage-1.hard-reboot = {
-    enable = mkOption {
-      type = types.bool;
-      default = true;
-      description = ''
-        Adds hard-reboot/hard-shutdown script.
-      '';
-    };
-  };
-
   config.mobile.boot.stage-1 = {
-    extraUtils = with pkgs; lib.mkIf cfg.hard-reboot.enable [
-      hard-reboot 
-      hard-shutdown
+    extraUtils = with pkgs; [
     ];
   };
 }
