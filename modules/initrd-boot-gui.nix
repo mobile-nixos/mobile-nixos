2b2d2ad7eacdf3d66ff97dc03b2e51df81abcbcb
Samuel Dionne-Riel <samuel@dionne-riel.com>
2020-02-29 15:36:36 -0500

    boot/applets: Add key-held utility

--- modules/initrd-boot-gui.nix
+++ modules/initrd-boot-gui.nix
@@ -2,6 +2,9 @@
 
 let
   boot-gui = ../boot/gui;
+  key-held = pkgs.runCommand "key-held.mrb" {} ''
+    ${pkgs.buildPackages.mruby}/bin/mrbc -o $out ${../boot/applets}/key-held.rb
+  '';
   boot-selection = pkgs.runCommand "boot-selection.mrb" {} ''
     ${pkgs.buildPackages.mruby}/bin/mrbc -o $out ${boot-gui}/lib/*.rb ${boot-gui}/main.rb
   '';
@@ -16,5 +19,13 @@ in
       object = boot-selection;
       symlink = "/applets/boot-selection.mrb";
     }
+    {
+      object = key-held;
+      symlink = "/applets/key-held.mrb";
+    }
+  ];
+  mobile.boot.stage-1.extraUtils = with pkgs; [
+    # Used for `key-held.mrb`.
+    { package = pkgsStatic.evtest; }
   ];
 }
