f1d828ece32c088757a9accc54822428659e4026
Samuel Dionne-Riel <samuel@dionne-riel.com>
2020-03-05 20:35:40 -0500

    rockchip: Add rk3399

--- modules/hardware-rockchip.nix
+++ modules/hardware-rockchip.nix
@@ -7,6 +7,11 @@ let
 in
 {
   options.mobile = {
+    hardware.socs.rockchip-rk3399.enable = mkOption {
+      type = types.bool;
+      default = false;
+      description = "enable when SOC is RK3399";
+    };
     hardware.socs.rockchip-op1.enable = mkOption {
       type = types.bool;
       default = false;
@@ -18,6 +23,12 @@ in
     {
       mobile = mkIf cfg.rockchip-op1.enable {
         system.system = "aarch64-linux";
+      };
+    }
+    {
+      mobile = mkIf cfg.rockchip-rk3399.enable {
+        system.system = "aarch64-linux";
+        quirks.u-boot.soc.family = "rockchip";
       };
     }
   ];
