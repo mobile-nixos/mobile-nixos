281f546bff270118369d4a5a745a26e2cc94da98
Samuel Dionne-Riel <samuel@dionne-riel.com>
2020-03-09 19:57:31 -0400

    stage-2-splash: Init re-purposed module
    This resets the VT for platforms with VTs.
    
    If a console is attached to that VT the logo will be covered by text as
    it begins showing up on the VT.
    
    The current way to "opt-out" of VT messages during stage-2 is to opt-out
    of console messages on the VT.

--- modules/stage-2-splash.nix
+++ modules/stage-2-splash.nix
@@ -1,10 +1,17 @@
 { config, lib, pkgs, ... }:
 
 {
-  # This happens in stage-2. This is why we're not using `addSplash`.
   # This is the earliest in stage-2 we can show, for vt-less devices, that
   # stage-2 is really happening.
   config.boot.postBootCommands = ''
+    # Reset the VT console.
+    # The Mobile NixOS stage-1 can be rude.
+    for d in /sys/class/vtconsole/vtcon*; do
+      if ${pkgs.busybox}/bin/grep 'frame buffer' "$d/name"; then
+        echo 1 > "$d/bind"
+      fi
+    done
+    # Though directly rudely show the stage-2 splash.
     ${pkgs.ply-image}/bin/ply-image --clear=0x000000 ${../artwork/splash.stage-2.png} > /dev/null 2>&1
   '';
 }
