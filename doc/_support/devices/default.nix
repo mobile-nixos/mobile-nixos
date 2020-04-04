96b82fc9f1d9e0aa0b5c51e7858a32f7a40da42e
Samuel Dionne-Riel <samuel@dionne-riel.com>
2020-04-04 17:01:24 -0400

    doc: Generate the device-specific docs via a script
    Rather than use ugly nix-templated bash.

--- doc/_support/devices/default.nix
+++ doc/_support/devices/default.nix
@@ -1,131 +1,33 @@
 { pkgs
 , stdenv
+, glibcLocales
 , runCommandNoCC
-, jq
 , symlinkJoin
+, ruby
 }:
 
 let
-  inherit (stdenv.lib) concatMapStrings;
-
   # Release tools used to evaluate the devices metadata.
   mobileReleaseTools = (import ../../../lib/release-tools.nix);
   inherit (mobileReleaseTools) all-devices;
   inherit (mobileReleaseTools.withPkgs pkgs) evalFor;
 
-  githubURL = "https://github.com/NixOS/mobile-nixos/tree/master/devices/";
-
   devicesDir = ../../../devices;
-  devices-info = symlinkJoin {
+  devicesInfo = symlinkJoin {
     name = "devices-metadata";
     paths = (map (device: (evalFor device).build.device-metadata) all-devices);
   };
-
-  tableColumns = [
-    { key = "identifier";   name = "Identifier"; }
-    { key = "manufacturer"; name = "Manufacturer"; }
-    { key = "name";         name = "Name"; }
-    { key = "hardware.soc"; name = "SoC"; }
-  ];
 in
 
 runCommandNoCC "mobile-nixos-docs-devices" {
   nativeBuildInputs = [
-    jq
+    ruby
+    glibcLocales
   ];
+  inherit devicesDir devicesInfo;
 }
 ''
   mkdir -p $out/devices
-  (cd $out
-  cat <<EOF > devices/index.adoc
-  = Devices List
-  include::_support/common.inc[]
-  :sitemap_index: true
-  :generated: true
-
-  The following table lists all devices Mobile NixOS available out of the
-  box on the master branch.
-
-  The inclusion in this list does not guarantee the device can boot Mobile
-  NixOS, but only that it did at one point in the past. Though, efforts are
-  made to ensure all of these still work.
-
-  [.with-links%autowidth]
-  |===
-  ${concatMapStrings ({name, ...}: "| ${name}") tableColumns}
-
-  EOF
-  )
-
-  (cd ${devices-info};
-  for d in *; do
-    get() {
-      jq -r ."$1" "$d"
-    }
-
-    identifier="$(get identifier)"
-    col() {
-      printf "<<$identifier.adoc#,%s>>\n" \
-        "$(get $1)"
-    }
-
-    deviceDoc="$out/devices/$identifier.adoc"
-
-    # Continue building the table for the index
-    printf "${concatMapStrings (_: "|%s\\n") tableColumns}\n" \
-    ${
-      concatMapStrings
-      ({key, ...}: ''"$(col ${key})" \${"\n"}'')
-      tableColumns
-    } >> $out/devices/index.adoc
-
-  # Make a per-device page
-  cat <<EOF > $deviceDoc
-  = $(get fullName)
-  include::_support/common.inc[]
-  :generated: true
-
-  [.device-sidebar]
-  .$(get fullName)
-  ****
-  ${""/* TODO: include picture if available. */}
-  Manufacturer:: $(get manufacturer)
-  Name:: $(get name)
-  Identifier:: $(get identifier)
-  System Type:: $(get system.type)
-  SoC:: $(get hardware.soc)
-  Architecture:: $(get system.system)
-  Source:: link:${githubURL}$identifier[Mobile NixOS repository]
-  ****
-
-  EOF
-
-    if [ -e "${devicesDir}/$identifier/README.adoc" ]; then
-      # FIXME: pattern match on the first empty line
-      tail -n +4 "${devicesDir}/$identifier/README.adoc" \
-        >> $deviceDoc.tmp
-
-      if [ "$(head -n1 $deviceDoc.tmp)" != "== Device-specific notes" ]; then
-        printf "Unexpected device-specific notes header for %s.\n\tGot: '%s'\n\tExpected: '%s'" "$identifier" "$(head -n1 $deviceDoc.tmp)" "== Device-specific notes"
-        exit 1
-      fi
-
-      cat $deviceDoc.tmp >> $deviceDoc
-      rm $deviceDoc.tmp
-    else
-      echo "_(No device-specific notes available)_" >> $deviceDoc
-    fi
-
-  done
-  )
-
-  (cd $out
-  cat <<EOF >> devices/index.adoc
-  |===
-
-  Remember to look at the link:https://github.com/NixOS/mobile-nixos/pulls?q=is%3Aopen+is%3Apr+label%3A%22type%3A+port%22[port label]
-  on the Mobile NixOS pull requests tracker, for upcoming devices.
-
-  EOF
-  )
+  export LC_CTYPE=en_US.UTF-8
+  ruby ${./generate-devices-listing.rb}
 ''
---
+++ doc/_support/devices/generate-devices-listing.rb
@@ -0,0 +1,112 @@
+require "json"
+
+def githubURL(device)
+  "https://github.com/NixOS/mobile-nixos/tree/master/devices/#{device}"
+end
+
+NOTES_HEADER = "== Device-specific notes"
+
+COLUMNS = [
+  { key: "identifier",   name: "Identifier" },
+  { key: "manufacturer", name: "Manufacturer" },
+  { key: "name",         name: "Name" },
+  { key: "hardware.soc", name: "SoC" },
+]
+
+$out = ENV["out"]
+$devicesInfo = Dir.glob(File.join(ENV["devicesInfo"], "*")).sort.map do |filename|
+  data = JSON.parse(File.read(filename))
+  [data["identifier"], data]
+end.to_h
+$devicesDir = ENV["devicesDir"]
+
+# First, generate the devices listing.
+puts ":: Generating devices/index.adoc"
+File.open(File.join($out, "devices/index.adoc"), "w") do |file|
+  file.puts <<~EOF
+  = Devices List
+  include::_support/common.inc[]
+  :sitemap_index: true
+  :generated: true
+
+  The following table lists all devices Mobile NixOS available out of the
+  box on the master branch.
+
+  The inclusion in this list does not guarantee the device can boot Mobile
+  NixOS, but only that it did at one point in the past. Though, efforts are
+  made to ensure all of these still work.
+
+  [.with-links%autowidth]
+  |===
+  #{COLUMNS.map {|col| "| #{col[:name]}" }.join("")}
+
+  EOF
+
+  $devicesInfo.keys.sort.each do |identifier|
+    data = $devicesInfo[identifier]
+    COLUMNS.each do |col|
+      value = data.dig(*(col[:key].split(".")))
+      if col[:key] == "identifier"
+        file.puts("|<<#{identifier}.adoc#,`#{value}`>>")
+      else
+        file.puts("|<<#{identifier}.adoc#,#{value}>>")
+      end
+    end
+  end
+
+  file.puts <<~EOF
+  |===
+
+  Remember to look at the link:https://github.com/NixOS/mobile-nixos/pulls?q=is%3Aopen+is%3Apr+label%3A%22type%3A+port%22[port label]
+  on the Mobile NixOS pull requests tracker, for upcoming devices.
+
+  EOF
+end
+
+# Then generate per-device pages
+$devicesInfo.values.each do |info|
+  identifier = info["identifier"]
+  puts ":: Generating devices/#{identifier}.adoc"
+  File.open(File.join($out, "devices/#{identifier}.adoc"), "w") do |file|
+    # TODO: include picture if available.
+
+    # Generate the side-bar
+    file.puts <<~EOF
+    = #{info["fullName"]}
+    include::_support/common.inc[]
+    :generated: true
+
+    [.device-sidebar]
+    .#{info["fullName"]}
+    ****
+    Manufacturer:: #{info["manufacturer"]}
+    Name:: #{info["name"]}
+    Identifier:: #{info["identifier"]}
+    System Type:: #{info["system"]["type"]}
+    SoC:: #{info["hardware"]["soc"]}
+    Architecture:: #{info["system"]["system"]}
+    Source:: link:#{githubURL(identifier)}[Mobile NixOS repository]
+    ****
+
+    EOF
+
+    # Generate the page contents
+    
+    deviceNotes = File.join($devicesDir, identifier, "README.adoc")
+    if File.exists?(deviceNotes)
+      notes = File.read(deviceNotes).split("\n\n", 2).last.strip
+      first_line = notes.lines.first.strip
+      unless first_line == NOTES_HEADER
+        $stderr.puts(
+          "Unexpected device-specific notes header for %s.",
+          "\tGot:      #{first_line.inspect}",
+          "\tExpected: #{NOTES_HEADER.inspect}",
+        )
+        exit 1
+      end
+      file.puts(notes)
+    else
+      file.puts("_(No device-specific notes available)_")
+    end
+  end
+end
