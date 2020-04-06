072562d77074f7748cc612ee21a9df0406664f6d
Samuel Dionne-Riel <samuel@dionne-riel.com>
2020-04-06 19:34:11 -0400

    doc: Add `rouge` to the converter gems

--- doc/_support/converter/gemset.nix
+++ doc/_support/converter/gemset.nix
@@ -9,4 +9,14 @@
     };
     version = "2.0.10";
   };
+  rouge = {
+    groups = ["default"];
+    platforms = [];
+    source = {
+      remotes = ["https://rubygems.org"];
+      sha256 = "0xl7k5paf66p57sphm4nfa4k86yf93lhdzzr0cv0l4divq12g2pr";
+      type = "gem";
+    };
+    version = "3.17.0";
+  };
 }
