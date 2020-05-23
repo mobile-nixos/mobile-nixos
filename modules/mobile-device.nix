ab35b1bf4c825eabf11a710f3b9d998a6ec9990b
Samuel Dionne-Riel <samuel@dionne-riel.com>
2020-05-23 17:28:36 -0400

    devices: Add device identity option

--- modules/mobile-device.nix
+++ modules/mobile-device.nix
@@ -6,11 +6,22 @@ with lib;
   options.mobile = {
     device.name = mkOption {
       type = types.str;
+      description = "The device's codename. Must match the device folder.";
     };
     device.info = mkOption {
       #type = types.attrSet;
       description = "system type specific informations";
       # This probably should be `internal`.
+    };
+    device.identity = {
+      name = mkOption {
+        type = types.str;
+        description = "The device's name as advertised by the manufacturer.";
+      };
+      manufacturer = mkOption {
+        type = types.str;
+        description = "The device's manufacturer name.";
+      };
     };
   };
 }
