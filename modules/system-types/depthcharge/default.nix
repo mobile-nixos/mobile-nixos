58e2a0709ff762db2952216e5b77fd8c4a187641
Samuel Dionne-Riel <samuel@dionne-riel.com>
2020-05-24 18:38:02 -0400

    system/depthcharge: Fix for new kernel package option

--- modules/system-types/depthcharge/default.nix
+++ modules/system-types/depthcharge/default.nix
@@ -2,15 +2,16 @@
 
 let
   inherit (lib) types;
+  inherit (config.mobile.boot) stage-1;
   enabled = config.mobile.system.type == "depthcharge";
 
   build = pkgs.callPackage ./depthcharge-build.nix {
-    inherit (config.mobile.device.info) kernel;
     inherit (config.mobile.system.depthcharge.kpart) dtbs;
     device_name = config.mobile.device.name;
     initrd = config.system.build.initrd;
     system = config.system.build.rootfs;
     cmdline = lib.concatStringsSep " " config.boot.kernelParams;
+    kernel = stage-1.kernel.package;
     arch = lib.strings.removeSuffix "-linux" config.mobile.system.system;
   };
 in
