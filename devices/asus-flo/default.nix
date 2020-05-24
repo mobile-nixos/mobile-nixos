5044aaeaef0560bc9adb0bea121d1f76dd11859e
Samuel Dionne-Riel <samuel@dionne-riel.com>
2020-05-24 02:43:35 -0400

    asus-flo: Fix build using new options

--- devices/asus-flo/default.nix
+++ devices/asus-flo/default.nix
@@ -10,19 +10,24 @@
   mobile.device.info = {
     # TODO : make kernel part of options.
     kernel = pkgs.callPackage ./kernel { kernelPatches = pkgs.defaultKernelPatches; };
-    dtb = "";
-    flash_offset_base = "0x80200000";
-    flash_offset_kernel = "0x00008000";
-    flash_offset_ramdisk = "0x02000000";
-    flash_offset_second = "0x00f00000";
-    flash_offset_tags = "0x00000100";
-    flash_pagesize = "2048";
   };
+
   mobile.hardware = {
     soc = "qualcomm-apq8064-1aa";
     ram = 1024 * 2;
     screen = {
       width = 1200; height = 1920;
+    };
+  };
+
+  mobile.system.android.bootimg = {
+    flash = {
+      offset_base = "0x80200000";
+      offset_kernel = "0x00008000";
+      offset_ramdisk = "0x02000000";
+      offset_second = "0x00f00000";
+      offset_tags = "0x00000100";
+      pagesize = "2048";
     };
   };
 
