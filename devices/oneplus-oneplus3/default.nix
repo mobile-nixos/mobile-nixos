2ac23a7828c16cb5a18040a99faa6cb45845e2fc
Samuel Dionne-Riel <samuel@dionne-riel.com>
2020-05-24 18:39:26 -0400

    oneplus-oneplus3: Fix for new kernel package option

--- devices/oneplus-oneplus3/default.nix
+++ devices/oneplus-oneplus3/default.nix
@@ -7,17 +7,16 @@
     manufacturer = "OnePlus";
   };
 
-  mobile.device.info = {
-    # TODO : make kernel part of options.
-    kernel = pkgs.callPackage ./kernel { kernelPatches = pkgs.defaultKernelPatches; };
-  };
-
   mobile.hardware = {
     soc = "qualcomm-msm8996";
     ram = 1024 * 6;
     screen = {
       width = 1080; height = 1920;
     };
+  };
+
+  mobile.boot.stage-1 = {
+    kernel.package = pkgs.callPackage ./kernel { kernelPatches = pkgs.defaultKernelPatches; };
   };
 
   mobile.system.android.bootimg = {
