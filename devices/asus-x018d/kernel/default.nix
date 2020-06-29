0e1d6c974b0086317bfc38a417800fab5960a2f7
Samuel Dionne-Riel <samuel@dionne-riel.com>
2020-06-28 21:14:47 -0400

    asus-x018d: Add kernel

---
+++ devices/asus-x018d/kernel/default.nix
@@ -0,0 +1,49 @@
+{
+  mobile-nixos
+, fetchFromGitHub
+, kernelPatches ? [] # FIXME
+, python2
+, buildPackages
+}:
+
+let
+  inherit (buildPackages) dtc;
+in
+(mobile-nixos.kernel-builder-gcc49 {
+  version = "3.18.35";
+  configfile = ./config.aarch64;
+
+  file = "Image.gz-dtb";
+  hasDTB = true;
+
+  src = fetchFromGitHub {
+    owner = "mobile-nixos";
+    repo = "linux";
+    rev = "0df1d3c38e1b8283f4497edc8b6b729c8da1e82b"; # mobile-nixos/asus-x018d
+    sha256 = "13ql3yn0syda2g0r1rhmrlwmcc9fc7rg87xdxbzs1np5hwmnj0m3";
+  };
+
+  patches = [
+    ./90_dtbs-install.patch
+    ./0001-mtkfb-Default-to-RGB-order.patch
+  ];
+
+  makeFlags = [
+    "DTC_EXT=${dtc}/bin/dtc"
+  ];
+
+  isModular = false;
+
+}).overrideAttrs({ postInstall ? "", postPatch ? "", nativeBuildInputs ? [], ... }: {
+  nativeBuildInputs = nativeBuildInputs ++ [
+    python2
+  ];
+  installTargets = [ "zinstall" "Image.gz-dtb" "install" ];
+  postPatch = postPatch + ''
+    patchShebangs tools
+  '';
+
+  postInstall = postInstall + ''
+    cp -v "$buildRoot/arch/arm64/boot/Image.gz-dtb" "$out/"
+  '';
+})
