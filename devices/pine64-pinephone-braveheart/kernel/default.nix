a9169a2495fc58ea76cc009034d22d31a635ce93
S Dao <si.dao@outlook.com>
2020-07-14 17:53:33 +0700

    device: pinephone: Rebase to kernel 5.6.y to fix issue related to pkg-config
      * Change `default.nix` to match kernel changeset, recalculate sha256 key
      * Change LEDs patch file since `sun50i-a64-pinephone.dts` is now replaced by `sun50i-a64-pinephone.dtsi`
    
    Signed-off-by: S Dao <si.dao@outlook.com>

--- devices/pine64-pinephone-braveheart/kernel/default.nix
+++ devices/pine64-pinephone-braveheart/kernel/default.nix
@@ -6,13 +6,13 @@
 }:
 
 (mobile-nixos.kernel-builder {
-  version = "5.5.0";
+  version = "5.6.0";
   configfile = ./config.aarch64;
   src = fetchFromGitLab {
     owner = "pine64-org";
     repo = "linux";
-    rev = "94cf851f0f4443c771a926102dee497def319b49";
-    sha256 = "1a4ch2j8hla3xd7rv38ra6bnv14lsnj0srhlh1c8vxxvwywzg815";
+    rev = "14c4d9ddc15f60645bd262b315fc7d770a44a1c6";
+    sha256 = "137l1y6g3lfmqhxxixdph42cy72398nlmbwmk4690w2anlj76f3s";
   };
   patches = [
     ./0001-dts-pinephone-Setup-default-on-and-panic-LEDs.patch
@@ -21,6 +21,6 @@
   installTargets = [ "install" "dtbs" ];
   postInstall = postInstall + ''
     mkdir -p "$out/dtbs/allwinner"
-    cp -v "$buildRoot/arch/arm64/boot/dts/allwinner/sun50i-a64-pinephone.dtb" "$out/dtbs/allwinner/"
+    cp -v "$buildRoot/arch/arm64/boot/dts/allwinner/sun50i-a64-pinephone*" "$out/dtbs/allwinner/"
   '';
 })
