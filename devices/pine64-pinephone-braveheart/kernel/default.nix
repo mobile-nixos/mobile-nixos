de00c9f4520645612b924e5ba8c52ded80fa0324
@noneucat <@noneucat>
2020-07-18 20:40:20 -0700

    pine64-pinephone-braveheart: linux 5.5 -> 5.7

--- devices/pine64-pinephone-braveheart/kernel/default.nix
+++ devices/pine64-pinephone-braveheart/kernel/default.nix
@@ -6,21 +6,21 @@
 }:
 
 (mobile-nixos.kernel-builder {
-  version = "5.5.0";
+  version = "5.7.0";
   configfile = ./config.aarch64;
   src = fetchFromGitLab {
     owner = "pine64-org";
     repo = "linux";
-    rev = "94cf851f0f4443c771a926102dee497def319b49";
-    sha256 = "1a4ch2j8hla3xd7rv38ra6bnv14lsnj0srhlh1c8vxxvwywzg815";
+    rev = "c906a7d36abaa9ca379015c275b85af5d5b01987";
+    sha256 = "0z4j6zd4w1miw5vsfwzxbcrqskav710vxkxbyjrbwmycpzc0jpkb";
   };
   patches = [
-    ./0001-dts-pinephone-Setup-default-on-and-panic-LEDs.patch
+    # ./0001-dts-pinephone-Setup-default-on-and-panic-LEDs.patch
   ];
 }).overrideAttrs({ postInstall ? "", ... }: {
   installTargets = [ "install" "dtbs" ];
   postInstall = postInstall + ''
     mkdir -p "$out/dtbs/allwinner"
-    cp -v "$buildRoot/arch/arm64/boot/dts/allwinner/sun50i-a64-pinephone.dtb" "$out/dtbs/allwinner/"
+    cp -v $buildRoot/arch/arm64/boot/dts/allwinner/sun50i-a64-pinephone-*.dtb $out/dtbs/allwinner/
   '';
 })
