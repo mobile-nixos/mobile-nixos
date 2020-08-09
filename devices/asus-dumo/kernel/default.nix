49d462a75d2e0a39368feae7df56e7697372d423
Samuel Dionne-Riel <samuel@dionne-riel.com>
2020-08-09 19:42:59 -0400

    asus-dumo: kernel 5.7 -> 5.8
    With a patch to work around a regression with sbs-battery

--- devices/asus-dumo/kernel/default.nix
+++ devices/asus-dumo/kernel/default.nix
@@ -6,7 +6,7 @@
 }:
 
 (mobile-nixos.kernel-builder {
-  version = "5.7.0";
+  version = "5.8.0";
   configfile = ./config.aarch64;
 
   hasDTB = true;
@@ -14,12 +14,15 @@
   src = fetchFromGitHub {
     owner = "torvalds";
     repo = "linux";
-    rev = "v5.7";
-    sha256 = "1z91r21gp9nnw2wr552xrrpm2gshq3vsdd9wj12pzhf7053wdcid";
+    rev = "v5.8";
+    sha256 = "1jffq83jzcvkvpf6afhwkaj0zlb293vlndp1r66xzx41mbnnra0x";
   };
 
   patches = [
     ./0001-gru-Force-hs200-for-eMMC.patch
+
+    # Work around Regression from https://lore.kernel.org/patchwork/project/lkml/list/?series=443749
+    ./0001-HACK-haphazard-revert-of-sbs-battery-improvements.patch
   ];
 
   isModular = false;
