78647e9a687bd4719f2f97ff62c88f94070a19f9
Samuel Dionne-Riel <samuel@dionne-riel.com>
2020-09-27 22:57:23 -0400

    sony-pioneer: Enable LED default triggers

--- devices/sony-pioneer/kernel/default.nix
+++ devices/sony-pioneer/kernel/default.nix
@@ -24,6 +24,7 @@ in
   };
 
   patches = [
+    ./0001-Mobile-NixOS-configure-LEDs.patch
     ./0001-mobile-nixos-Adds-and-sets-BGRA-as-default.patch
     ./0001-mobile-nixos-Workaround-selected-processor-does-not-.patch
     ./0003-arch-arm64-Add-config-option-to-fix-bootloader-cmdli.patch
