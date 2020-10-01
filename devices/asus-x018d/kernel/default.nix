c15684ddca7d8af4f6721a3649894227d65f6a45
Samuel Dionne-Riel <samuel@dionne-riel.com>
2020-10-01 02:16:20 -0400

    asus-x018d: Update usage of kernel builder

--- devices/asus-x018d/kernel/default.nix
+++ devices/asus-x018d/kernel/default.nix
@@ -6,15 +6,9 @@
 , buildPackages
 }:
 
-let
-  inherit (buildPackages) dtc;
-in
-(mobile-nixos.kernel-builder-gcc49 {
+mobile-nixos.kernel-builder-gcc49 {
   version = "3.18.35";
   configfile = ./config.aarch64;
-
-  file = "Image.gz-dtb";
-  hasDTB = true;
 
   src = fetchFromGitHub {
     owner = "mobile-nixos";
@@ -32,23 +26,11 @@ in
     ./0002-E262L-Green-LED-now-defaults-to-on.patch
   ];
 
-  makeFlags = [
-    "DTC_EXT=${dtc}/bin/dtc"
-  ];
-
-  isModular = false;
-
-  postPatch = ''
-    patchShebangs tools
-  '';
-
   nativeBuildInputs = [
+    # Needed for tools/dct/DrvGen.py
     python2
   ];
-}).overrideAttrs({ postInstall ? "", ... }: {
-  installTargets = [ "zinstall" "Image.gz-dtb" "install" ];
 
-  postInstall = postInstall + ''
-    cp -v "$buildRoot/arch/arm64/boot/Image.gz-dtb" "$out/"
-  '';
-})
+  isImageGzDtb = true;
+  isModular = false;
+}
