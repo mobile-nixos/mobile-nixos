e9487fd116294abf915fcf2bc8345f579b314b5d
Samuel Dionne-Riel <samuel@dionne-riel.com>
2020-10-09 18:46:15 -0400

    razer-cheryl2: Disable some of the default command line parameters
    They are causing trouble during boot

--- devices/razer-cheryl2/default.nix
+++ devices/razer-cheryl2/default.nix
@@ -37,18 +37,18 @@
 
   boot.kernelParams = [
     # Extracted from an Android boot image
-    "console=ttyMSM0,115200n8"
-    "earlycon=msm_geni_serial,0xA84000"
+    #"console=ttyMSM0,115200n8"
+    #"earlycon=msm_geni_serial,0xA84000"
     "androidboot.hardware=qcom"
     "androidboot.console=ttyMSM0"
-    "video=vfb:640x400,bpp=32,memsize=3072000"
+    #"video=vfb:640x400,bpp=32,memsize=3072000"
     "msm_rtb.filter=0x237"
     "ehci-hcd.park=3"
     "lpm_levels.sleep_disabled=1"
     "service_locator.enable=1"
     "swiotlb=2048"
     "androidboot.configfs=true"
-    "firmware_class.path=/vendor/firmware_mnt/image"
+    #"firmware_class.path=/vendor/firmware_mnt/image"
     "loop.max_part=7"
     "androidboot.usbcontroller=a600000.dwc3"
     "buildvariant=user"
