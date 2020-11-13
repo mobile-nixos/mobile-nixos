263d6713c957301842031f5e6e500bab8ddfd8f6
Samuel Dionne-Riel <samuel@dionne-riel.com>
2020-11-12 21:26:58 -0500

    boot/error: Give a choice for the next action to the end-user

--- boot/error/main.rb
+++ boot/error/main.rb
@@ -2,6 +2,7 @@ begin
 
 data = JSON.parse(File.read(ARGV.first))
 
+$exit = false
 $code = data["code"]
 $color = data["color"]
 $delay = data["delay"]
@@ -12,6 +13,9 @@ $title = data["title"]
 $color = $color.rjust(6, "0").rjust(8, "F").to_i(16)
 
 class UI
+  # As this is not using BaseWindow, LVGUI::init isn't handled for us.
+  LVGUI.init()
+
   def initialize()
     add_screen
 
@@ -152,6 +156,37 @@ class UI
     @time_left.set_y(padding)
   end
 
+  def set_actions(actions)
+    @action_buttons ||= []
+    @action_buttons.each do |el|
+      el.del()
+    end
+
+    y_position = @time_left.get_y() + @time_left.get_height() + padding()
+
+    @action_buttons = actions.map do |action_pair|
+      label, action = action_pair
+
+      LVGUI::Button.new(@actions_pane).tap do |btn|
+        LVGUI.focus_group.add_obj(btn)
+        btn.glue_obj(true)
+        btn.set_label(label)
+        btn.set_y(y_position)
+        btn.event_handler = ->(event) do
+          case event
+          when LVGL::EVENT::CLICKED
+            action.call()
+          end
+        end
+
+        # For the next item
+        y_position = btn.get_y() + btn.get_height() + padding()
+      end
+    end
+
+    relayout()
+  end
+
   # Layout helpers
 
   def relayout()
@@ -200,7 +235,16 @@ class UI
 
   # Updates the UI with the time left.
   def set_time_left(value)
-    @time_left.set_text("#{value} seconds left before crashing.")
+    if value
+      @time_left.set_text("#{value} seconds left before crashing.")
+    else
+      @time_left.set_text("Select an option:")
+    end
+  end
+
+  def hide_actions()
+    @actions_pane.set_opa_scale_enable(true)
+    @actions_pane.set_opa_scale(0)
   end
 
   def get_pane_width()
@@ -229,26 +273,70 @@ class UI
 end
 
 # Create the UI
-ui = UI.new
+$ui = UI.new
 
 # Run tasks once to "realize" the UI.
 LVGL::Hacks::LVTask.handle_tasks
 
+def run(*cmd)
+  $stderr.puts " $ " + cmd.join(" ")
+  system(*cmd) unless LVGL::Introspection.simulator?
+end
+
+def cleanup_and_exit()
+  $ui.hide_actions()
+
+  # Ensure the next scheduled handle_tasks will run
+  LVGL::Hacks::LVTask.handle_tasks
+  sleep(0.1)
+
+  # Refresh the UI one last time.
+  LVGL::Hacks::LVTask.handle_tasks
+
+  puts "Exiting error applet!"
+  puts "exit(#{$status})"
+
+  # Ensures console is flushed entirely.
+  $stdout.flush()
+  $stderr.flush()
+
+  # Ensures all kernel work is done before it kernel panics at exit.
+  sleep(0.1)
+
+  # Exit, which will crash the kernel.
+  exit $status
+end
+
+$ui.set_actions([[
+  "Cancel time-out", ->() do
+    $delay = nil
+    $ui.set_time_left(nil)
+
+    $ui.set_actions([
+      ["Power off", ->() { run("poweroff") }],
+      ["Kernel panic", ->() { $exit = true }],
+      *(RebootModes.options),
+    ])
+  end
+]])
+
 start = Time.now
 LVGUI.main_loop do
-  elapsed = Time.now - start
-  left = $delay - elapsed
+  if $delay
+    elapsed = Time.now - start
+    left = $delay - elapsed
+    $ui.set_time_left(left.floor)
 
-  ui.set_time_left(left.floor)
-
-  if elapsed >= $delay
-    # Ensures console is flushed entirely.
-    $stdout.flush()
-    $stderr.flush()
-
-    # Exit, which will crash the kernel.
-    exit $status
+    if elapsed >= $delay
+      $exit = true
+    end
   end
+
+  # We're using `$exit` as a flag to ensure we exit after all event handlers
+  # have been completed.
+  # This is because we will not be able to force a render of the UI during
+  # an event handler.
+  cleanup_and_exit() if $exit
 end
 
 # Handles outputing the error and, more importantly, flushing the output.
