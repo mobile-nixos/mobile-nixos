6a785b19e5c531eade443e1b078c938a14e8009b
Samuel Dionne-Riel <samuel@dionne-riel.com>
2020-12-15 19:08:45 -0500

    system-types/depthcharge: Use stage-0

--- modules/system-types/depthcharge/default.nix
+++ modules/system-types/depthcharge/default.nix
@@ -1,17 +1,19 @@
 { config, pkgs, lib, ... }:
 
 let
-  inherit (lib) types;
-  inherit (config.mobile.boot) stage-1;
   enabled = config.mobile.system.type == "depthcharge";
+
+  inherit (lib) types;
+  inherit (config.system.build) stage-0;
+  inherit (stage-0.mobile.boot.stage-1) kernel;
 
   build = pkgs.callPackage ./depthcharge-build.nix {
     inherit (config.mobile.system.depthcharge.kpart) dtbs;
     device_name = config.mobile.device.name;
-    initrd = config.system.build.initrd;
+    initrd = stage-0.system.build.initrd;
     system = config.system.build.rootfs;
     cmdline = lib.concatStringsSep " " config.boot.kernelParams;
-    kernel = stage-1.kernel.package;
+    kernel = kernel.package;
     arch = lib.strings.removeSuffix "-linux" config.mobile.system.system;
   };
 in
