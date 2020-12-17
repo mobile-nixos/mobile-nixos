0ece77292bcc47dcb967374af1b1573924a40db2
Samuel Dionne-Riel <samuel@dionne-riel.com>
2020-12-17 18:13:28 -0500

    stage-0: defaults according to platform and kernel version
    AArch64 Linux (arm64) didnd't support kexec up until 4.8

--- modules/stage-0.nix
+++ modules/stage-0.nix
@@ -1,4 +1,4 @@
-{ config, lib, modules, baseModules, ... }:
+{ config, lib, pkgs, modules, baseModules, ... }:
 
 # This module provides the `stage-0` build output.
 # It is the same configuration, with minor customizations.
@@ -6,14 +6,23 @@
 let 
   inherit (lib) mkOption types;
   inherit (config.mobile.quirks) supportsStage-0;
+  kernelVersionSupportsKexec = 
+    if pkgs.targetPlatform.system == "aarch64-linux"
+    then (lib.versionAtLeast (config.mobile.boot.stage-1.kernel.package.version) "4.8")
+    else true
+  ;
 in
 {
   options = {
     mobile.quirks.supportsStage-0 = mkOption {
       type = types.bool;
-      default = true;
+      default = kernelVersionSupportsKexec;
+      defaultText = "(Varies per platform; aarch64 depends on kernel version.)";
       description = ''
         Set to false when a device cannot use `kexec` to kexec into a system.
+
+        A default value will be selected according to the platform and
+        `mobile.boot` kernel selected.
       '';
     };
   };
