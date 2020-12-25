8650b4c3a6e0a63b1018f4de4daca2cacf9f0567
Samuel Dionne-Riel <samuel@dionne-riel.com>
2020-12-25 15:45:44 -0500

    pine64-pinephone: kernel 5.9 -> 5.10

--- devices/pine64-pinephone/kernel/default.nix
+++ devices/pine64-pinephone/kernel/default.nix
@@ -4,15 +4,15 @@
 }:
 
 mobile-nixos.kernel-builder {
-  version = "5.9.0";
+  version = "5.10.0";
   configfile = ./config.aarch64;
   src = fetchFromGitHub {
     # https://github.com/megous/linux
     owner = "megous";
     repo = "linux";
-    # orange-pi-5.9
-    rev = "e98db7f114d7602c6b847d76e183787f0c97cf5b";
-    sha256 = "007j5r0ygy7sfs7d49qx623irl8a9rl7ppl9159jj419izplrzyf";
+    # orange-pi-5.10
+    rev = "cf48c321c3ebd42d33234bf5fc1b7f8b4de86c95";
+    sha256 = "0p6gnxc80m7l3mw89fljpysayf269b09xk88z4nyfv92z0gn6yiv";
   };
   patches = [
     ./0001-dts-pinephone-Setup-default-on-and-panic-LEDs.patch
