ac315d791f2a22ffa7fe46b676d978f92fd95106
Samuel Dionne-Riel <samuel@dionne-riel.com>
2020-12-31 16:57:39 -0500

    mruby-lvgui: drop package

--- overlay/mruby-builder/overlay.nix
+++ overlay/mruby-builder/overlay.nix
@@ -6,15 +6,10 @@ let
   # Let's co-opt the `dontDisableStatic` attribute it overrides into a
   # derivation, let's say, hello...
   static = if final.hello ? dontDisableStatic then final.hello.dontDisableStatic else false;
-  mrbgems = final.callPackage ./mrbgems {};
 in
 {
   hello-mruby = final.callPackage ./hello-mruby {};
-  mrbgems = mrbgems // {
-    mruby-lvgui = final.callPackage ./mrbgems/mruby-lvgui {
-      mrbgems = mrbgems;
-    };
-  };
+  mrbgems = final.callPackage ./mrbgems {};
   mruby = (final.callPackage ./mruby {}).overrideAttrs({passthru ? {}, ...}: {
     passthru = passthru // {
       builder = final.callPackage ./mruby/builder.nix {
