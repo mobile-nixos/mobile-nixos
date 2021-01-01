bec41be25bd58de0949a1c564f9debb115812b6d
Samuel Dionne-Riel <samuel@dionne-riel.com>
2021-01-01 17:49:38 -0500

    boot/splash: describe libraries used

--- boot/splash/default.nix
+++ boot/splash/default.nix
@@ -3,6 +3,19 @@
 , mruby
 }:
 
+let
+  inherit (lib) concatMapStringsSep;
+
+  # Select libs we need from the libs folder.
+  libs = concatMapStringsSep " " (name: "${../lib}/${name}") [
+    "lvgui/args.rb"
+    "lvgui/fiddlier.rb"
+    "lvgui/lvgl/*.rb"
+    "lvgui/lvgui/*.rb"
+    "lvgui/vtconsole.rb"
+  ];
+in
+
 # mkDerivation will append something like -aarch64-unknown-linux-gnu to the
 # derivation name with cross, which will break the mruby code loading.
 # Since we don't need anything from mkDerivation, really, let's use runCommand.
@@ -16,6 +29,7 @@ runCommand "boot-splash.mrb" {
 } ''
   mrbc \
     -o $out \
+    ${libs} \
     $(find $lib -type f -name '*.rb' | sort) \
     $(find $src/lib -type f -name '*.rb' | sort) \
     $src/main.rb
