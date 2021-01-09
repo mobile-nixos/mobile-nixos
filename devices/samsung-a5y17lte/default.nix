55d684ebdf013a143c1c8adde30a8f73f3508467
Samuel Dionne-Riel <samuel@dionne-riel.com>
2021-01-09 00:10:51 -0500

    samsung-a5y17lte: Init Samsung Galaxy A5 (2017)

---
+++ devices/samsung-a5y17lte/default.nix
@@ -0,0 +1,43 @@
+{ pkgs, ... }:
+
+{
+  mobile.device.name = "samsung-a5y17lte";
+  mobile.device.identity = {
+    name = "Galaxy A5 (2017)";
+    manufacturer = "Samsung";
+  };
+
+  mobile.hardware = {
+    soc = "exynos-7880";
+    ram = 1024 * 3;
+    screen = {
+      width = 1080; height = 1920;
+    };
+  };
+
+  mobile.boot.stage-1 = {
+    kernel.package = pkgs.callPackage ./kernel { };
+  };
+
+  mobile.system.android = {
+    bootimg.flash = {
+      offset_base = "0x10000000";
+      offset_kernel = "0x00008000";
+      offset_ramdisk = "0x01000000";
+      offset_second = "0x00f00000";
+      offset_tags = "0x00000100";
+      pagesize = "2048";
+    };
+  };
+
+  boot.kernelParams = [
+    # Extracted from an Android boot image
+    # (Actually blank!)
+  ];
+
+  mobile.system.type = "android";
+
+  mobile.usb.mode = "android_usb";
+  mobile.usb.idVendor = "04E8"; # Samsung
+  mobile.usb.idProduct = "6860"; # Galaxy A
+}
