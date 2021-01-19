cb49e49b75fba995a791baa001f6f140ec72e50d
Samuel Dionne-Riel <samuel@dionne-riel.com>
2021-01-18 21:51:36 -0500

    examples/hello: Simplify main window implementation

--- examples/hello/app/windows/main.rb
+++ examples/hello/app/windows/main.rb
@@ -1,15 +1,11 @@
 module GUI
   class MainWindow < LVGUI::BaseWindow
+    include LVGUI::BaseUIElements
     include LVGUI::ButtonPalette
     def initialize()
       super()
 
-      LVGL::LVLabel.new(@container).tap do |label|
-        label.set_long_mode(LVGL::LABEL_LONG::BREAK)
-        label.set_text(%Q{Your device booted to\nstage-2 of a NixOS system successfully!\n\nSelect from the following options})
-        label.set_align(LVGL::LABEL_ALIGN::CENTER)
-        label.set_width(@container.get_width_fit)
-      end
+      add_main_text(%Q{Your device booted to\nstage-2 of a NixOS system successfully!\n\nSelect from the following options})
 
       add_buttons([
         ["Display validation", ->() { DisplayValidationWindow.instance.present }],
