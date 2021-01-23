8cba4282426eab605351faf512157acf9f2c5162
Samuel Dionne-Riel <samuel@dionne-riel.com>
2021-01-22 22:56:41 -0500

    examples/hello: Add select to LVGUI testing page

--- examples/hello/app/windows/lvguitesting.rb
+++ examples/hello/app/windows/lvguitesting.rb
@@ -8,15 +8,34 @@ module GUI
     def initialize()
       super()
 
-      add_main_text(%Q{The different UI toolkit controls are shown here.\n\nThis is used to validate UI behaviour on different platforms.\n\n})
+      add_main_text(%Q{The different UI toolkit controls are shown here.\n\nThis is used to validate UI behaviour on different platforms.\n})
+
+      LVGUI::HorizontalSeparator.new(@container)
+
+      add_main_text(%Q{\nThe description label here will mirror the switch's value.\n})
 
       # Add a toggle switch
       @switch = add_switch(
         "Toggle switch",
-        description: "This label will mirror its value in the description field after the first activation.",
+        description: "true",
         initial: true,
       ) do |new_state|
         @switch.set_description(new_state.inspect)
+      end
+
+      LVGUI::HorizontalSeparator.new(@container)
+
+      add_main_text(%Q{\nThese items allow choosing among many options\n})
+
+      @select = add_select("Options selection", [
+        [:A, "Option A"],
+        [:B, "Option B"],
+        [:C, "Option C"],
+        [:D, "Option D"],
+        #[:_Z, "An option with a label that is large enough to cause a line wrap"],
+        #[:_X, "Commas wrap snuggly,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,"],
+      ]) do |new_state|
+        p new_state
       end
     end
   end
