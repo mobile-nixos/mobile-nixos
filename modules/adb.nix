6f6940226a2c564a1fd75dd3d09f32f59d714f4c
Samuel Dionne-Riel <samuel@dionne-riel.com>
2021-01-27 22:14:03 -0500

    modules/adb: ADB works just fine on any usb gadget
    (Though it may require fiddling with udev perms)

--- modules/adb.nix
+++ modules/adb.nix
@@ -15,9 +15,6 @@ with lib;
 
   config = lib.mkIf config.mobile.adbd.enable {
     assertions = [
-      { assertion = config.mobile.system.type == "android";
-        message = "adb is only available on Android";
-      }
       { assertion = config.mobile.boot.stage-1.usb.enable;
         message = "adb requires mobile.boot.stage-1.usb.enable = true";
       }
