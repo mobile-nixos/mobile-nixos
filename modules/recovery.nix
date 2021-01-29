84b7e5eead58d9f9e8e8b07e0507a44ecb9bc05f
Samuel Dionne-Riel <samuel@dionne-riel.com>
2021-01-28 21:32:02 -0500

    modules/recovery: Use composeConfig

--- modules/recovery.nix
+++ modules/recovery.nix
@@ -1,16 +1,15 @@
-{ pkgs, modules, baseModules, _mobile-nixos, ... }:
+{ config, pkgs, ... }:
 
 # This module provides the `recovery` build output.
 # It is the same configuration, with minor customizations.
 
 {
-  system.build.recovery = (_mobile-nixos.evalConfig {
-    inherit baseModules;
-    modules = modules ++ [{
+  system.build.recovery = (config.lib.mobile-nixos.composeConfig {
+    config = {
       mobile.system.android.bootimg.name = "recovery.img";
       mobile.boot.stage-1.bootConfig = {
         is_recovery = true;
       };
-    }];
+    };
   }).config;
 }
