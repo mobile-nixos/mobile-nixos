cd2287be998e7746138a90e815e41f342834f21d
Samuel Dionne-Riel <samuel@dionne-riel.com>
2021-01-30 23:07:57 -0500

    asus-dumo: Move MMC fixup task into a file
    It shouldn't have been made inline from the beginning.

--- devices/asus-dumo/default.nix
+++ devices/asus-dumo/default.nix
@@ -59,51 +59,7 @@
   };
 
   mobile.boot.stage-1.tasks = [
-    # This hack unbinds and rebinds the currently problematic storage driver.
-    # TODO: move into a generic "gru family" thing.
-    (pkgs.writeText "fixup-sdhci-arasan.rb" ''
-      class Tasks::FixupSDHCIArasan < SingletonTask
-        MAX = 60;
-        NAME = "fe330000.sdhci"
-        DRIVER = "/sys/bus/platform/drivers/sdhci-arasan"
-        GLOB = "#{DRIVER}/#{NAME}/mmc_host/mmc*/mmc*/block"
-
-        def initialize()
-          add_dependency(:Mount, "/sys")
-          add_dependency(:Files, DRIVER)
-          #add_dependency(:Target, :Environment)
-        end
-
-        def run()
-          tries = 0
-
-          $stdout.print " -> Waiting for #{NAME}"
-          $stdout.flush
-          until Dir.glob(GLOB).length > 0 do
-            $stdout.print "."
-            $stdout.flush
-            tries += 1
-            begin
-              System.write(File.join(DRIVER, "unbind"), NAME)
-            rescue => e
-              $logger.fatal(e.inspect)
-            end
-            begin
-              System.write(File.join(DRIVER, "bind"), NAME)
-            rescue => e
-              $logger.fatal(e.inspect)
-            end
-
-            sleep(1)
-            raise "Couldn't get #{NAME} up in #{tries} tries." if tries > MAX
-          end
-          $stdout.puts "!\n"
-
-          log("Took #{tries} tries for #{NAME} to appear...")
-        end
-      end
-    '')
-
+    ./fixup_sdhci_arasan_task.rb
     ./usb_role_switch_task.rb
   ];
 }
