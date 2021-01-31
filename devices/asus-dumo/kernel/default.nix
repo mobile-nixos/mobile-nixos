6dfa19f5ff1d7d694d28ae62c10bea62950ecd14
Samuel Dionne-Riel <samuel@dionne-riel.com>
2021-01-30 21:47:21 -0500

    asus-dumo: kernel 5.8 -> 5.10

--- devices/asus-dumo/kernel/default.nix
+++ devices/asus-dumo/kernel/default.nix
@@ -5,21 +5,18 @@
 }:
 
 mobile-nixos.kernel-builder {
-  version = "5.8.0";
+  version = "5.10.0";
   configfile = ./config.aarch64;
 
   src = fetchFromGitHub {
     owner = "torvalds";
     repo = "linux";
-    rev = "v5.8";
-    sha256 = "1jffq83jzcvkvpf6afhwkaj0zlb293vlndp1r66xzx41mbnnra0x";
+    rev = "v5.10";
+    sha256 = "1znxp4v7ykfz4fghzjzhd5mj9pj5qpk88n7k7nbkr5x2n0xqfj6k";
   };
 
   patches = [
     ./0001-gru-Force-hs200-for-eMMC.patch
-
-    # Work around Regression from https://lore.kernel.org/patchwork/project/lkml/list/?series=443749
-    ./0001-HACK-haphazard-revert-of-sbs-battery-improvements.patch
   ];
 
   isModular = false;
