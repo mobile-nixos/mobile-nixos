715ea5927be93facc2d6388ba153c035bb231a83
Samuel Dionne-Riel <samuel@dionne-riel.com>
2021-02-11 16:27:23 -0500

    shell.nix: Add dtc and ubootTools

--- shell.nix
+++ shell.nix
@@ -8,10 +8,12 @@ mkShell rec {
     mobile-nixos.autoport     # Helps users kickstart their ports
 
     # Third party tools
+    dtc                       # For playing around with device tree files
     dtbTool                   # Combines multiple device tree blobs into one image
     file                      # Shows the type of files
     lz4                       # Decompress image files
     mkbootimg                 # Pack and unpack boot images
     python3Packages.binwalk   # Search a binary image for embedded files
+    ubootTools                # A couple useful utilities
   ];
 }
