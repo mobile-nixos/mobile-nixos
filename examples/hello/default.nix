5b49dcf20802ee2f6a5a76464b60955452dd5cb0
Samuel Dionne-Riel <samuel@dionne-riel.com>
2021-02-21 17:31:56 -0500

    examples/hello: Use eval-with-configuration.nix

--- examples/hello/default.nix
+++ examples/hello/default.nix
@@ -1,22 +1,11 @@
-{ device ? null }:
+{ device ? null, pkgs ? null }@args:
 
-let
-  system-build = import ../../. {
-    inherit device;
-    configuration = [ { imports = [ ./configuration.nix ]; } ];
-  };
-in
-  system-build // {
-    ___readme-default = throw ''
-    Cannot directly build for ${device}...
-
+import ../../lib/eval-with-configuration.nix (args // {
+  configuration = [ (import ./configuration.nix) ];
+  additionalHelpInstructions = ''
     You can build the `-A build.default` attribute to build the default output
     for your device.
 
      $ nix-build examples/hello --argstr device ${device} -A build.default
-
-    *************************************************************************
-    * Please also read your device's documentation for further usage notes. *
-    *************************************************************************
   '';
-  }
+})
