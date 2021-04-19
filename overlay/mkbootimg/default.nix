f88dbbb978bb07027c217a40ecadccb0b8f7332c
Rick van Schijndel <rol3517@gmail.com>
2021-04-19 20:48:58 +0200

    mkbootimg: support cross-compilation

--- overlay/mkbootimg/default.nix
+++ overlay/mkbootimg/default.nix
@@ -9,6 +9,15 @@ stdenv.mkDerivation rec {
     sha256 = "1zz06x31rak9sv5v01kj3cvqvzh5qvq34p00paw5p3jl0lxvnvwc";
   };
 
+  # Using makeFlags doesn't work due to the args being passed as well
+  # Replacing is required for cross-compilation
+  postPatch = ''
+    substituteInPlace Makefile \
+      --replace "AR = ar rcv" "AR = ${stdenv.cc.bintools.targetPrefix}ar rcv"
+    substituteInPlace libmincrypt/Makefile \
+      --replace "AR = ar rc" "AR = ${stdenv.cc.bintools.targetPrefix}ar rc"
+  '';
+
   installPhase = ''
     mkdir -p $out/bin
     cp -v mkbootimg $out/bin/
