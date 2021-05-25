26839fbc8fd92c64803785e36dbb7499c5861310
Daniel Barlow <dan@telent.net>
2021-05-25 21:37:52 +0100

    switch to boulzordev kernel, de-androidify the config

--- devices/motorola-potter/kernel/default.nix
+++ devices/motorola-potter/kernel/default.nix
@@ -15,20 +15,22 @@
 #
 
 mobile-nixos.kernel-builder-gcc6 {
-  version = "3.18.71";
+  version = "3.18.113";
   configfile = ./. + "/config.${stdenv.hostPlatform.parsed.cpu.name}";
 
   src = fetchFromGitHub {
-    owner = "LineageOS";
+    owner = "boulzordev";
     repo = "android_kernel_motorola_msm8953";
-    rev = "80530de6e297dd0f0ba479c0dcc4ddb7c9e90e24"; # lineage-15.1
-    sha256 = "0qw8x61ycpkk5pqvs9k2abr5lq56ga5dml6vkygvmi8psm2g6kg1";
+    rev = "efeb95ed634079eb520922f6f78f16f9d967a6d4";
+    sha256 = "03xmlyq06fvn0v4nvrs2nhmb88axg45ccd1vb3nam6jj1381zv4w";
   };
 
   patches = [
     ./04_fix_camera_msm_isp.patch
-    ./05_misc_msm_fixes.patch
-    ./06_prima_gcc6.patch
+    # these two don't apply to boulzordev kernel, and I am
+    # hoping/assuming are not needed
+    #    ./05_misc_msm_fixes.patch
+    #    ./06_prima_gcc6.patch
     ./99_framebuffer.patch
     ./0001-Allow-building-WCD9335_CODEC-without-REGMAP_ALLOW_WR.patch
     ./0005-Allow-building-with-sound-disabled.patch
