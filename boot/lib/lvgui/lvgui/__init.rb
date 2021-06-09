1127cee44f07542d62f47d48e8ac35357d75aa6a
Samuel Dionne-Riel <samuel@dionne-riel.com>
2021-06-08 21:36:58 -0400

    lvgui: Extract the base widget

--- boot/lib/lvgui/lvgui/__init.rb
+++ boot/lib/lvgui/lvgui/__init.rb
@@ -1,2 +1,16 @@
 module LVGUI
+  # Wraps an LVGL widget.
+  class Widget
+    def initialize(widget)
+      @widget = widget
+    end
+    def method_missing(*args)
+      @widget.send(*args)
+    end
+
+    # Needed to make respond_to? work.
+    def lv_obj_pointer()
+      @widget.lv_obj_pointer
+    end
+  end
 end
--- boot/lib/lvgui/lvgui/gui.rb
+++ boot/lib/lvgui/lvgui/gui.rb
@@ -117,21 +117,6 @@ module LVGUI
     end
   end
 
-  # Wraps an LVGL widget.
-  class Widget
-    def initialize(widget)
-      @widget = widget
-    end
-    def method_missing(*args)
-      @widget.send(*args)
-    end
-
-    # Needed to make respond_to? work.
-    def lv_obj_pointer()
-      @widget.lv_obj_pointer
-    end
-  end
-
   # Used mainly to create an intangible object that the focus ring can default
   # on so it doesn't focus anything by default.
   class Dummy < Widget
