b4e6f98fc6caae584c0ff932a532393c0fefd076
Samuel Dionne-Riel <samuel@dionne-riel.com>
2021-06-13 17:57:48 -0400

    boot/lib: Make reboot modes HAL more robust
    Allows running in the simulator.

--- boot/lib/hal/reboot_modes.rb
+++ boot/lib/hal/reboot_modes.rb
@@ -1,15 +1,27 @@
 module Hal
   module RebootModes
+    extend self
+
     Android = {
       "recovery"   => ["Reboot to recovery", ->() { run("reboot recovery") }],
       "bootloader" => ["Reboot to bootloader", ->() { run("reboot bootloader") }],
     }
 
-    def self.options()
+    def reboot_modes()
+      if Configuration["HAL"] && 
+         Configuration["HAL"]["boot"] && 
+         Configuration["HAL"]["boot"]["rebootModes"]
+         Configuration["HAL"]["boot"]["rebootModes"]
+      else
+        []
+      end
+    end
+
+    def options()
       [
         ["Reboot to system", ->() { run("reboot") }],
       ] + 
-        Configuration["HAL"]["boot"]["rebootModes"].map do |identifier|
+        reboot_modes.map do |identifier|
           const, key = identifier.split(".", 2)
           const_get(const)[key]
         end
