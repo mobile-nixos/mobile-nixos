07e96623cc42ea0c9bca6fda89bec738077a3299
Samuel Dionne-Riel <samuel@dionne-riel.com>
2021-06-13 17:37:11 -0400

    examples/target-diskmode: Use mkLVGUIApp

--- examples/target-disk-mode/app/default.nix
+++ examples/target-disk-mode/app/default.nix
@@ -1,39 +1,10 @@
-{ runCommandNoCC
-, lib
-, mruby
-}:
+{ lib, mobile-nixos }:
 
-let
-  inherit (lib) concatMapStringsSep;
-
-  # Select libs we need from the libs folder.
-  libs = concatMapStringsSep " " (name: "${../../../boot/lib}/${name}") [
-    "lvgui/args.rb"
-    "lvgui/fiddlier.rb"
-    "lvgui/lvgl/*.rb"
-    "lvgui/lvgui/*.rb"
-    "lvgui/mobile_nixos/*.rb"
-    "lvgui/vtconsole.rb"
-  ];
-in
-
-runCommandNoCC "tdm-gui.mrb" {
+mobile-nixos.mkLVGUIApp {
+  name = "tdm-gui.mrb";
   src = lib.cleanSource ./.;
-
-  nativeBuildInputs = [
-    mruby
+  rubyFiles = [
+    "$(find ./windows -type f -name '*.rb' | sort)"
+    "main.rb"
   ];
-} ''
-  cp -prf $src src
-  cd src
-
-  mkdir -p $out/libexec/
-  mrbc \
-    -o $out/libexec/app.mrb \
-    ${libs} \
-    $(find ./windows -type f -name '*.rb' | sort) \
-    main.rb
-
-  # TODO: add ressources here?
-  mkdir -p $out/share/tdm-gui
-''
+}
