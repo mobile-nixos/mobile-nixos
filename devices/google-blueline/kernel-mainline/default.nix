ed31a6ae3efd257813ca57bdae44d57982201c47
Samuel Dionne-Riel <samuel@dionne-riel.com>
2021-06-30 20:57:35 -0400

    google-blueline: Update mainline-based rev for one with touchscreen
    Some recent development.

--- devices/google-blueline/kernel-mainline/default.nix
+++ devices/google-blueline/kernel-mainline/default.nix
@@ -11,11 +11,11 @@ mobile-nixos.kernel-builder rec {
 
   # FIXME: apply patchsets directly on top of mainline
   src = fetchFromGitHub {
-    # Mirror of http://git.linaro.org/people/vinod.koul/kernel.git/commit/?h=pixel/dsc_rfc&id=dcf128e83ddd25e461a5080a349de5a0552eed32
+    # Mirror of http://git.linaro.org/people/vinod.koul/kernel.git/commit/?h=pixel/gpi_i2c_touch&id=1b3424a2994b82ac38fdf16e2136e7811548fea4
     owner = "samueldr";
     repo = "linux";
-    rev = "dcf128e83ddd25e461a5080a349de5a0552eed32";
-    sha256 = "07xp9bi0xl54yfyspaw2aa410142xvy38p2l6z92ximajlgxh1gv";
+    rev = "1b3424a2994b82ac38fdf16e2136e7811548fea4";
+    sha256 = "1ml9br4p88z1whgqfmbv9vc0jn94mgy6jyg1p69nmn05h3wzim63";
   };
 
   patches = [
