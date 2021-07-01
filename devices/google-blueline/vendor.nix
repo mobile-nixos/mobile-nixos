476827769aa50733c34512b3377dc67a815b6f47
Samuel Dionne-Riel <samuel@dionne-riel.com>
2021-07-01 16:28:16 -0400

    google-blueline: vendor: Provide touch panel firmware in stage-1
    This is used to recover from bad flashes from the mainline driver.

--- devices/google-blueline/vendor.nix
+++ devices/google-blueline/vendor.nix
@@ -1,8 +1,22 @@
 { config, lib, pkgs, ... }:
 
+let
+  # Reduced set of firmware files, only for the touch panel.
+  # This helps fix a broken touch panel firmware storage from testing mainline.
+  firmware-stage-1 = pkgs.runCommandNoCC "google-blueline-firmware-stage-1" {} ''
+    mkdir -p $out/lib/firmware/
+    cp -vt $out/lib/firmware/ ${config.mobile.device.firmware}/lib/firmware/ftm5*.ftb
+  '';
+in
+
 lib.mkIf (config.mobile.boot.stage-1.kernel.provenance == "vendor")
 {
   mobile.boot.stage-1 = {
     kernel.package = pkgs.callPackage ./kernel-vendor { };
   };
+
+  mobile.device.firmware = pkgs.callPackage ./firmware-vendor { };
+  mobile.boot.stage-1.firmware = [
+    firmware-stage-1
+  ];
 }
