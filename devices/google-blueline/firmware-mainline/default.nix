7decb58366b8818d5761034ec9434260fc717156
Samuel Dionne-Riel <samuel@dionne-riel.com>
2021-07-01 17:39:51 -0400

    google-blueline: Make mainline firmware use vendor firmware files

--- devices/google-blueline/firmware-mainline/default.nix
+++ devices/google-blueline/firmware-mainline/default.nix
@@ -1,38 +1,9 @@
-{ lib
-, fetchurl
-, runCommandNoCC
+{ runCommandNoCC
 , firmwareLinuxNonfree
 , wireless-regdb
-, unzip
-, e2fsprogs
-, simg2img
+, vendor-firmware-files
 }:
 
-let
-  # https://dl.google.com/dl/android/aosp/blueline-rq3a.210605.005-factory-53820251.zip
-  buildID = "rq3a.210605.005";
-  upstreamImage = fetchurl {
-    url = "https://dl.google.com/dl/android/aosp/blueline-${buildID}-factory-53820251.zip";
-    sha256 = "17l4b5gs8g182czl3zyvs8kydb6w23hbwkd2m9ngy7wfym8h50jk";
-  };
-
-  vendor-firmware-files = runCommandNoCC "google-blueline-firmware" {
-    nativeBuildInputs = [ unzip e2fsprogs simg2img ];
-    meta.license = [
-      # We make no claims that it can be redistributed.
-      lib.licenses.unfree
-    ];
-  } ''
-    unzip ${upstreamImage} blueline-${buildID}/image-blueline-${buildID}.zip
-    cd blueline-${buildID}
-    unzip image-blueline-${buildID}.zip vendor.img
-    simg2img vendor.img vendor-raw.img
-    debugfs vendor-raw.img -R "rdump firmware ."
-
-    mkdir -p $out/lib
-    mv firmware $out/lib/firmware
-  '';
-in
 # The minimum set of firmware files required for the device.
 runCommandNoCC "google-blueline-firmware" {
   src = firmwareLinuxNonfree;
--- devices/google-blueline/mainline.nix
+++ devices/google-blueline/mainline.nix
@@ -6,7 +6,9 @@ lib.mkIf (config.mobile.boot.stage-1.kernel.provenance == "mainline")
     kernel.package = pkgs.callPackage ./kernel-mainline { };
   };
 
-  mobile.device.firmware = pkgs.callPackage ./firmware-mainline {};
+  mobile.device.firmware = pkgs.callPackage ./firmware-mainline {
+    vendor-firmware-files = pkgs.callPackage ./firmware-vendor { };
+  };
   mobile.boot.stage-1.firmware = [
     config.mobile.device.firmware
   ];
