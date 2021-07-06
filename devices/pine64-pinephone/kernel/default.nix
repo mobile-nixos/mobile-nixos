1f9cd1177420a4f19e6d47cd0d8ae0b66751e869
Tom Fitzhenry <tom@tom-fitzhenry.me.uk>
2021-07-06 22:39:50 +1000

    pine64-pinephone: upgrade to megous's 5.13 kernel

--- devices/pine64-pinephone/kernel/default.nix
+++ devices/pine64-pinephone/kernel/default.nix
@@ -5,15 +5,15 @@
 }:
 
 mobile-nixos.kernel-builder {
-  version = "5.11.2";
+  version = "5.13.0";
   configfile = ./config.aarch64;
   src = fetchFromGitHub {
     # https://github.com/megous/linux
     owner = "megous";
     repo = "linux";
-    # orange-pi-5.11
-    rev = "945310589fa62cb25f54e885c4c3f43b31fa009b";
-    sha256 = "1ygha48bvm14dahlni0wrc3r3c8pc5dj3bdhmvqx2lrb52mfiasc";
+    # orange-pi-5.13
+    rev = "f5627aedb8c84fe0509285ef1f03fbdfc8d06ce5";
+    sha256 = "0f8bsvv9f11b499dp049i8h6rbhnfnm4xyz1nhwpvjh4qm51jajq";
   };
   patches = [
     ./0001-dts-pinephone-Setup-default-on-and-panic-LEDs.patch
