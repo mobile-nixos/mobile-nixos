79a900c9c1c7651c9bd7d0b7ec8a33b388d8fe06
Samuel Dionne-Riel <samuel@dionne-riel.com>
2021-07-21 22:09:04 -0400

    lvgui: Add header/text label helpers

--- boot/lib/lvgui/lvgui/windows.rb
+++ boot/lib/lvgui/lvgui/windows.rb
@@ -30,6 +30,17 @@ module LVGUI
 
   module BaseUIElements
     def add_main_text(text, alignment: LVGL::LABEL_ALIGN::CENTER)
+      add_text(text, alignment: alignment)
+    end
+
+    def add_header(text)
+      LVGUI::HeaderLabel.new(@container).tap do |label|
+        label.text = text
+        label.set_width(@container.get_width_fit)
+      end
+    end
+
+    def add_text(text, alignment: LVGL::LABEL_ALIGN::LEFT)
       LVGL::LVLabel.new(@container).tap do |label|
         label.set_long_mode(LVGL::LABEL_LONG::BREAK)
         label.set_text(text)
