55eba3f5a46eeed6aeb844bbbef013892d4e2816
Samuel Dionne-Riel <samuel@dionne-riel.com>
2021-07-28 22:36:13 -0400

    examples/installer: Also use `lib` folder

--- examples/installer/app/default.nix
+++ examples/installer/app/default.nix
@@ -5,6 +5,7 @@ mobile-nixos.mkLVGUIApp {
   src = lib.cleanSource ./.;
   enableDebugInformation = true;
   rubyFiles = [
+    "$(find ./lib -type f -name '*.rb' | sort)"
     "$(find ./gui -type f -name '*.rb' | sort)"
     "$(find ./windows -type f -name '*.rb' | sort)"
     "main.rb"
