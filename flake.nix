6419d2b2aeeb3ca7af14c927f0e50c49cbc05817
ostylk <ostylk@googlemail.com>
2021-08-27 12:33:54 +0200

    flakes: added device specific configurations as nixosModules

--- flake.nix
+++ flake.nix
@@ -6,6 +6,9 @@
   outputs = { self, nixpkgs }:
     let
       supportedSystems = [ "x86_64-linux" "i686-linux" "aarch64-linux" ];
+      supportedDevices = builtins.filter
+        (device: builtins.pathExists(./. + "/devices/${device}/default.nix"))
+        (builtins.attrNames (builtins.readDir ./devices));
 
       forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
 
@@ -18,6 +21,14 @@
         default = import ./overlay/overlay.nix;
         mruby-builder = import ./overlay/mruby-builder/overlay.nix;
       };
+
+      nixosModules = builtins.listToAttrs (builtins.map
+        (device:
+          {
+            name = device;
+            value = (import ./lib/configuration.nix { inherit device; });
+          })
+        supportedDevices);
 
       legacyPackages = forAllSystems (system: nixpkgsFor.${system});
     };
