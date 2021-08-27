d67f968711eb3eaaa12e3b38b750164a6a3b4cac
Samuel Dionne-Riel <samuel@dionne-riel.com>
2021-08-27 01:20:42 -0400

    examples/hello: build.* -> outputs.*

--- examples/hello/default.nix
+++ examples/hello/default.nix
@@ -3,9 +3,9 @@
 import ../../lib/eval-with-configuration.nix (args // {
   configuration = [ (import ./configuration.nix) ];
   additionalHelpInstructions = ''
-    You can build the `-A build.default` attribute to build the default output
+    You can build the `-A outputs.default` attribute to build the default output
     for your device.
 
-     $ nix-build examples/hello --argstr device ${device} -A build.default
+     $ nix-build examples/hello --argstr device ${device} -A outputs.default
   '';
 })
