e5cb86b5eef769528467cbf49e1117ada30d227e
Samuel Dionne-Riel <samuel@dionne-riel.com>
2021-09-24 22:13:55 -0400

    mruby-zmq: update

--- overlay/mruby-builder/mrbgems/default.nix
+++ overlay/mruby-builder/mrbgems/default.nix
@@ -376,15 +376,11 @@ rec {
     src = fetchFromGitHub {
       repo = "mruby-zmq";
       owner = "zeromq";
-      rev = "39b6dab7cb944595064ca3e9376637024d3bf483";
-      sha256 = "03n9890wx18v7pwnk5w8s10l7yij6hyj8y5q4jf296k56dr4g01m";
+      rev = "ed45cef4d6f813978a0c3cc60b5f62208af7cf71";
+      sha256 = "0vx0akd4kmb1iwvxd5dn8q65f6f3paw2rswxy8h23lgf6yxyks3h";
     };
 
     patches = [
-      (fetchpatch {
-        url = "https://github.com/zeromq/mruby-zmq/pull/16.patch";
-        sha256 = "1m63yl84hg80whqpr3yznk06yalaasiszwm3pjvsr2di8gyi0bm5";
-      })
       ./mruby-zmq/0001-Work-around-missing-pthread.patch
       ./mruby-zmq/0001-HACK-cross-build-is-not-special-with-Nixpkgs.patch
     ];
