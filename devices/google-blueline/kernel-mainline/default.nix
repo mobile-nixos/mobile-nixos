00ede51076737fcba7ae2e2abd1ce79c7f453465
Samuel Dionne-Riel <samuel@dionne-riel.com>
2021-10-09 22:18:07 -0400

    google-blueline: Update DSC patch to v2

--- devices/google-blueline/kernel-mainline/default.nix
+++ devices/google-blueline/kernel-mainline/default.nix
@@ -23,9 +23,9 @@ mobile-nixos.kernel-builder rec {
   patches = [
     ./0001-HACK-Add-back-TEXT_OFFSET-in-the-built-image.patch
 
-    # http://git.linaro.org/people/vinod.koul/kernel.git/log/?h=pixel/dsc_v1
-    # @ 8e0150ddba7a61d981acbb288a59fcea7e444129
-    ./0001-Linaro-blueline-dsc-v1-wip.patch
+    # http://git.linaro.org/people/vinod.koul/kernel.git/log/?h=pixel/dsc_v2
+    # @ 7b8fcc2fcf1792a035ef35715a7db133a8d22635 
+    ./0001-Linaro-blueline-dsc-v2-wip.patch
 
     # https://git.linaro.org/people/vinod.koul/kernel.git/log/?h=pixel/gpi_i2c_touch
     # @ 1b3424a2994b82ac38fdf16e2136e7811548fea4
