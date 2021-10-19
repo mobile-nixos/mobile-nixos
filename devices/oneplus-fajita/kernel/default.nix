07d96669b6687f9096107a6b864776e96e6a480e
Miles Breslin <milesbreslin@gmail.com>
2021-10-19 00:38:24 -0700

    oneplus-fajita: Unstable kernel commit

--- devices/oneplus-fajita/kernel/default.nix
+++ devices/oneplus-fajita/kernel/default.nix
@@ -6,14 +6,14 @@
 }:
 
 mobile-nixos.kernel-builder rec {
-  version = "5.12.15";
-  modDirVersion = "5.12.15";
+  version = "5.14.0-rc6";
+  modDirVersion = "5.14.0-rc6";
   configfile = ./config.aarch64;
   src = fetchFromGitLab {
     owner = "sdm845-mainline";
     repo = "linux";
-    rev = "089737886ff3b793ccece369b88ee0ae4b39b9f5";
-    sha256 = "sha256-TM+pdweCX/VhKnjusvBUdpgPdQibywvSnsYAWI0mL8w=";
+    rev = "94a1a791d2559c8224a6dbd58a0a7e59d7c85712";
+    sha256 = "sha256-8+bGuaXPv45NndRbzA0nSy/qazibdIp1joLADXNoW2g=";
   };
 
   patches = [
