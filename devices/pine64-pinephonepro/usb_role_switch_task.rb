58d9c57bf50a02da6b47342befa2c50dd70c3cb1
Samuel Dionne-Riel <samuel@dionne-riel.com>
2021-12-14 20:55:52 -0500

    pine64-pinephonepro: Add USB role switch task
    This is used to force the type-c direction.

---
+++ devices/pine64-pinephonepro/usb_role_switch_task.rb
@@ -0,0 +1,14 @@
+class Tasks::UsbRoleSwitchTask < SingletonTask
+  def initialize()
+    Tasks::SetupGadgetMode.instance.add_dependency(:Task, self)
+    add_dependency(:Mount, "/sys")
+  end
+
+  # Toggle the role switch, it has been observed (by others online) that it
+  # *may* help in some situations, compared to just setting it to "device".
+  def run()
+    System.write("/sys/class/usb_role/fe800000.usb-role-switch/role", "host")
+    sleep(0.1)
+    System.write("/sys/class/usb_role/fe800000.usb-role-switch/role", "device")
+  end
+end
