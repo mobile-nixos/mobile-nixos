494cdc5f2457352e4e85bf92b5a231240bf29624
Samuel Dionne-Riel <samuel@dionne-riel.com>
2021-12-14 19:37:06 -0500

    hardware-rockchip: Add rockchip-rk3399s

--- modules/hardware-rockchip.nix
+++ modules/hardware-rockchip.nix
@@ -1,8 +1,7 @@
 { config, lib, pkgs, ... }:
 
-with lib;
-
 let
+  inherit (lib) mkIf mkMerge mkOption types;
   cfg = config.mobile.hardware.socs;
 in
 {
@@ -12,11 +11,21 @@ in
       default = false;
       description = "enable when SOC is RK3399-OP1";
     };
+    hardware.socs.rockchip-rk3399s.enable = mkOption {
+      type = types.bool;
+      default = false;
+      description = "enable when SOC is RK3399";
+    };
   };
 
   config = mkMerge [
     {
       mobile = mkIf cfg.rockchip-op1.enable {
+        system.system = "aarch64-linux";
+      };
+    }
+    {
+      mobile = mkIf cfg.rockchip-rk3399s.enable {
         system.system = "aarch64-linux";
       };
     }
