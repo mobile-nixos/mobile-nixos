2cdeca4b08081b0fadb04a26b3b95961f51ccdfe
Tom Fitzhenry <tom@tom-fitzhenry.me.uk>
2022-05-07 01:35:15 +1000

    pine64-pinephone: kernel 5.17.0-rc7 -> 5.17.5

--- devices/pine64-pinephone/kernel/default.nix
+++ devices/pine64-pinephone/kernel/default.nix
@@ -6,15 +6,15 @@
 }:
 
 mobile-nixos.kernel-builder {
-  version = "5.17.0-rc7";
+  version = "5.17.5";
   configfile = ./config.aarch64;
   src = fetchFromGitHub {
     # https://github.com/megous/linux
     owner = "megous";
     repo = "linux";
     # orange-pi-5.17
-    rev = "2e2c23ad09b5c78042dc989d07fcd30afa7a99c8";
-    sha256 = "sha256-cjvC6LP4oill+iXWR1nqgF1JvuOp4VDcnKc9tSngMEs=";
+    rev = "3508d5fd2af1b74c370f86863aa99933537826b6";
+    sha256 = "sha256-KfA9MeZL1cogkXYn8n2Vuk+W5S8EaX9e1q03JTH8YGI=";
   };
   patches = [
     ./0001-dts-pinephone-Setup-default-on-and-panic-LEDs.patch
