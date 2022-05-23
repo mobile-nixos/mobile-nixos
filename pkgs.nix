1fcde06594c3850c4626b1416b93e5e40fa654e2
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-05-23 15:34:09 -0400

    pkgs.nix: Update pin

--- pkgs.nix
+++ pkgs.nix
@@ -1,6 +1,6 @@
 let
-  sha256 = "sha256:1wg55jlxyvbjvm8x2rcirmvqws4y8xq504dn3yjp05m1bajhpj5r";
-  rev = "6daa4a5c045d40e6eae60a3b6e427e8700f1c07f";
+  sha256 = "sha256:1ipd1k1gvxh9sbg4w4cpa3585q09gvsq8xbjvxnnmfjib6r6xx4i";
+  rev = "dfd82985c273aac6eced03625f454b334daae2e8";
 in
 builtins.trace "(Using pinned Nixpkgs at ${rev})"
 import (fetchTarball {
