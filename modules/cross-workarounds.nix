d2d3f1132612820cc875f57d1b0a2caba1eaa530
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-05-23 18:26:14 -0400

    cross-workarounds: Drop unneeded workarounds

--- modules/cross-workarounds.nix
+++ modules/cross-workarounds.nix
@@ -16,16 +16,7 @@ lib.mkIf isCross (lib.mkMerge [
 
 # All cross-compilation
 {
-  # building '/nix/store/eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee-fc-cache.drv'...
-  # [...]-fontconfig-2.10.2-aarch64-unknown-linux-gnu-bin/bin/fc-cache: cannot execute binary file: Exec format error
-  fonts.fontconfig.enable = false;
-
-  # building '/nix/store/eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee-mesa-19.3.3-aarch64-unknown-linux-gnu.drv'...
-  # meson.build:1537:2: ERROR: Dependency "wayland-scanner" not found, tried pkgconfig
-  security.polkit.enable = false;
-
-  # udisks fails due to gobject-introspection being not cross-compilation friendly.
-  services.udisks2.enable = false;
+  # No workarounds needed!
 }
 
 # 32 bit ARM
@@ -34,22 +25,14 @@ lib.mkIf isCross (lib.mkMerge [
     (final: super:
       # Ensure pkgsBuildBuild ends up unmodified, otherwise the canary test will
       # get super expensive to build.
-      if super.stdenv.buildPlatform == super.stdenv.hostPlatform then {} else {
-      # Works around libselinux failure with python on armv7l.
-      # LONG_BIT definition appears wrong for platform
-      libselinux = (super.libselinux
-        .override({
-          enablePython = false;
-        }))
-        .overrideAttrs (_: {
-          preInstall = ":";
-        })
-      ;
-
-      # btrfs-progs-armv7l-unknown-linux-gnueabihf-5.17.drv
-      # /nix/store/wnrc4daqbd6v5ifqlxsj75ky8556zy0p-python3-3.9.12/include/python3.9/pyport.h:741:2: error: #error "LONG_BIT definition appears wrong for platform (bad gcc/glibc config?)."
-      btrfs-progs = lib.warn "btrfs-progs neutered due to broken build with cross armv7l" nullPackage;
-    })
+      if super.stdenv.buildPlatform == super.stdenv.hostPlatform
+      then {}
+      else {
+        # btrfs-progs-armv7l-unknown-linux-gnueabihf-5.17.drv
+        # /nix/store/wnrc4daqbd6v5ifqlxsj75ky8556zy0p-python3-3.9.12/include/python3.9/pyport.h:741:2: error: #error "LONG_BIT definition appears wrong for platform (bad gcc/glibc config?)."
+        btrfs-progs = lib.warn "btrfs-progs neutered due to broken build with cross armv7l" nullPackage;
+      }
+    )
   ];
 })
 
