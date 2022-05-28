6aa9f6e7a56ce8b7ad4ee4b6280eca102b3918b1
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-05-27 20:16:15 -0400

    modules/hardware-screen: Add missing options description

--- modules/hardware-screen.nix
+++ modules/hardware-screen.nix
@@ -9,9 +9,15 @@ in
   options.mobile.hardware.screen = {
     width = mkOption {
       type = types.int;
+      description = ''
+        Width of the device's display.
+      '';
     };
     height = mkOption {
       type = types.int;
+      description = ''
+        Height of the device's display.
+      '';
     };
   };
 }
