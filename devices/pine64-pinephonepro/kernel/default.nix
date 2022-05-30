e8ffe9b29c74e27c25f55161c6e952ef97870c3e
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-05-30 15:07:50 -0400

    [HACK] pine64-pinephonepro: Add workaround to show the logo
    This serves more to prove to me that it actually should be working. What
    needs to actually be done is figure out *what* is preventing the logo
    from showing.
    
    Note that this is not a Pinephone Pro issue, nor is it an RK3399 issue,
    nor is it an ARM issue. This workaround had been authored for an x86_64
    system initially.

--- devices/pine64-pinephonepro/kernel/default.nix
+++ devices/pine64-pinephonepro/kernel/default.nix
@@ -17,6 +17,7 @@ mobile-nixos.kernel-builder {
   };
 
   patches = [
+    ./0001-HACK-always-show-logo.patch
     ./0001-arm64-dts-rockchip-set-type-c-dr_mode-as-otg.patch
     ./0001-dts-pinephone-pro-Setup-default-on-and-panic-LEDs.patch
     ./0001-usb-dwc3-Enable-userspace-role-switch-control.patch
