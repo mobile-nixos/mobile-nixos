10d5aa9cd4cb80e172df416d1e16eacfb6cd2604
Brian McKenna <brian@brianmckenna.org>
2022-07-06 12:11:05 +1000

    kobo-clara: kernel 5.16.0 -> 5.17.9

--- devices/kobo-clara/kernel/default.nix
+++ devices/kobo-clara/kernel/default.nix
@@ -1,17 +1,20 @@
 {
   mobile-nixos
 , fetchFromGitHub
+, lzop
 , ...
 }:
 
 mobile-nixos.kernel-builder {
-  version = "5.16.0";
+  version = "5.17.9";
   configfile = ./config.armv7l;
 
   src = fetchFromGitHub {
     owner = "akemnade";
     repo = "linux";
-    rev = "9be9fd52253701860b03e0d31557021943d7e0a0";
-    sha256 = "1idqpynhifll9hq4m5kv38z19kkk4222zw6sa3a5lxvrai4484l1";
+    rev = "ad6a7e9da33a3c6dd317420728dd2b0ea16716f8";
+    sha256 = "sha256-JTNJTLq2L2zVeJkSQuhmSYii3m5MnlGuSIw60wDnqcg=";
   };
+
+  nativeBuildInputs = [ lzop ];
 }
