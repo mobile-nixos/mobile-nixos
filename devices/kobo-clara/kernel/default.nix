04b75cb1d608c1353a5f244ce47cce05fc261286
Brian McKenna <brian@brianmckenna.org>
2022-07-11 11:45:32 +1000

    kobo-clara: use NixOS kernel builder
    This is necessary for compiling the WiFi module.

--- devices/kobo-clara/kernel/default.nix
+++ devices/kobo-clara/kernel/default.nix
@@ -1,20 +1,23 @@
 {
-  mobile-nixos
+  linuxManualConfig
 , fetchFromGitHub
 , lzop
+, stdenv
+, lib
 , ...
 }:
 
-mobile-nixos.kernel-builder {
+(linuxManualConfig {
+  inherit stdenv lib;
   version = "5.17.9";
-  configfile = ./config.armv7l;
-
   src = fetchFromGitHub {
     owner = "akemnade";
     repo = "linux";
     rev = "ad6a7e9da33a3c6dd317420728dd2b0ea16716f8";
     sha256 = "sha256-JTNJTLq2L2zVeJkSQuhmSYii3m5MnlGuSIw60wDnqcg=";
   };
-
-  nativeBuildInputs = [ lzop ];
-}
+  configfile = ./config.armv7l;
+  config = import ./config.armv7l.nix;
+}).overrideAttrs (attrs: {
+  nativeBuildInputs = attrs.nativeBuildInputs ++ [ lzop ];
+})
