a61f7c764661f3909bc5bf29a0d345655f09b324
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-09-12 22:24:25 -0400

    oneplus-enchilada: Use SDM845 family semantics

--- devices/oneplus-enchilada/default.nix
+++ devices/oneplus-enchilada/default.nix
@@ -2,7 +2,7 @@
 
 {
   imports = [
-    ./sound.nix
+    ../families/sdm845-mainline
   ];
 
   mobile.device.name = "oneplus-enchilada";
@@ -13,62 +13,13 @@
   mobile.device.supportLevel = "supported";
 
   mobile.hardware = {
-    soc = "qualcomm-sdm845";
     ram = 1024 * 8;
     screen = {
       width = 1080; height = 2280;
     };
   };
 
-  mobile.boot.stage-1 = {
-    compression = "xz";
-    kernel.package = pkgs.callPackage ./kernel { };
-  };
-
   mobile.device.firmware = pkgs.callPackage ./firmware {};
-  hardware.enableRedistributableFirmware = true;
-  hardware.firmware = lib.mkBefore [ config.mobile.device.firmware ];
-  mobile.boot.stage-1.firmware = [
-    # NOTE: putting the full firmware files here risks making the initramfs
-    # too big, which is known to break boot.
-    # Having the firmware files only in the built system is sufficient.
-    # config.mobile.device.firmware
-  ];
 
   mobile.system.android.device_name = "OnePlus6";
-  mobile.system.android = {
-    ab_partitions = true;
-    bootimg.flash = {
-      offset_base = "0x00000000";
-      offset_kernel = "0x00008000";
-      offset_ramdisk = "0x01000000";
-      offset_second = "0x00f00000";
-      offset_tags = "0x00000100";
-      pagesize = "4096";
-    };
-  };
-
-  boot.kernelParams = [
-    "console=tty0"
-  ];
-
-  mobile.usb.mode = "gadgetfs";
-  # Google
-  mobile.usb.idVendor = "18D1";
-  # "Nexus 4"
-  mobile.usb.idProduct = "D001";
-
-  mobile.system.type = "android";
-
-
-  mobile.usb.gadgetfs.functions = {
-    adb = "ffs.adb";
-    rndis = "rndis.usb0";
-  };
-
-  mobile.quirks.qualcomm.sdm845-modem.enable = true;
-
-  services.udev.extraRules = ''
-    SUBSYSTEM=="input", KERNEL=="event*", ENV{ID_INPUT}=="1", SUBSYSTEMS=="input", ATTRS{name}=="pmi8998_haptics", TAG+="uaccess", ENV{FEEDBACKD_TYPE}="vibra"
-  '';
 }
