2b0a272c142cf2797d9fa4a0fd2b58971f60252f
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-09-12 22:38:43 -0400

    oneplus-fajita: Use SDM845 family semantics
    Also directly use the same firmware package as enchilada.

--- devices/oneplus-fajita/default.nix
+++ devices/oneplus-fajita/default.nix
@@ -1,6 +1,10 @@
 { config, lib, pkgs, ... }:
 
 {
+  imports = [
+    ../families/sdm845-mainline
+  ];
+
   mobile.device.name = "oneplus-fajita";
   mobile.device.identity = {
     name = "OnePlus 6T";
@@ -10,61 +14,13 @@
   mobile.device.supportLevel = "best-effort";
 
   mobile.hardware = {
-    soc = "qualcomm-sdm845";
     ram = 1024 * 8;
     screen = {
       width = 1080; height = 2340;
     };
   };
 
-  mobile.boot.stage-1 = {
-    compression = "xz";
-    kernel.package = pkgs.callPackage ./kernel { };
-    firmware = [
-      config.mobile.device.firmware
-    ];
-  };
-
-
-  mobile.device.firmware = pkgs.callPackage ./firmware {};
+  mobile.device.firmware = pkgs.callPackage ../oneplus-enchilada/firmware {};
 
   mobile.system.android.device_name = "OnePlus6T";
-  mobile.system.android = {
-    ab_partitions = true;
-    bootimg.flash = {
-      offset_base = "0x00000000";
-      offset_kernel = "0x00008000";
-      offset_ramdisk = "0x01000000";
-      offset_second = "0x00f00000";
-      offset_tags = "0x00000100";
-      pagesize = "4096";
-    };
-  };
-
-  boot.kernelParams = [
-    "console=tty0"
-  ];
-
-  mobile.usb.mode = "gadgetfs";
-  # Google
-  mobile.usb.idVendor = "18D1";
-  # "Nexus 4"
-  mobile.usb.idProduct = "D001";
-
-  mobile.system.type = "android";
-
-  hardware.enableRedistributableFirmware = true;
-  hardware.firmware = lib.mkBefore [ config.mobile.device.firmware ];
-
-
-  mobile.usb.gadgetfs.functions = {
-    adb = "ffs.adb";
-    rndis = "rndis.usb0";
-  };
-
-  mobile.quirks.qualcomm.sdm845-modem.enable = true;
-
-  services.udev.extraRules = ''
-    SUBSYSTEM=="input", KERNEL=="event*", ENV{ID_INPUT}=="1", SUBSYSTEMS=="input", ATTRS{name}=="pmi8998_haptics", TAG+="uaccess", ENV{FEEDBACKD_TYPE}="vibra"
-  '';
 }
