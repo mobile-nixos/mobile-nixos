5db79e595629129751ea18e01966a91783beb7c1
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-09-13 21:17:14 -0400

    XXX: google-blueline: 5.19.7 rebase of 20795113b74df511fd599d3867a6bd1a5b0f9c48

--- devices/google-blueline/kernel/default.nix
+++ devices/google-blueline/kernel/default.nix
@@ -1,25 +1,22 @@
 { mobile-nixos
 , fetchFromGitHub
-, fetchFromGitLab
 , ...
 }:
 
 mobile-nixos.kernel-builder rec {
-  version = "5.19.0";
+  version = "5.19.7";
   configfile = ./config.aarch64;
 
-  src = fetchFromGitLab {
-    owner = "sdm845-mainline";
+  src = fetchFromGitHub {
+    owner = "samueldr";
     repo = "linux";
-    rev = "20795113b74df511fd599d3867a6bd1a5b0f9c48"; # XXX caleb/pixel3-bringup-5.19 DO NOT SHIP
-    hash = "sha256-rPvDe+xgClToIvKKbhC1y/fxeZnD+carx8zYy5KdsJ0=";
+    rev = "864d93dba25dbc459f235836b954dd644e05ebfb"; # XXX WIP
+    hash = "sha256-nui9eFD6GIYWBUhmwoQSLnbIG88P2hpUlPl9b2E/Rrg=";
   };
 
   patches = [
-    # Present in sd845-mainline WIP bringup branch already
-    # ./0001-HACK-Add-back-TEXT_OFFSET-in-the-built-image.patch
-    ./0001-touchscreen-focaltech_fts-Add-missing-include.patch
     ./0001-XXX-google-blueline-sync-dts-with-9060b7256952a63311.patch
+    ./0001-touchscreen-focaltech_fts-Add-missing-include.patch
   ];
 
   isModular = false;
