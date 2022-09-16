f1c76da3bd1dafc5c162293f0e4871a33e3cfcf8
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-09-16 17:50:28 -0400

    examples/installer: Add abstraction glue for configuration data

---
+++ examples/installer/app/lib/configuration.rb
@@ -0,0 +1,35 @@
+# "Broker" for the configuration data.
+# This somewhat decouples the installation bits from the internal structure,
+# even though in the end we're relying on the internal structure from the steps
+# windows.
+module Configuration
+  extend self
+
+  DESCRIPTION = [
+    { path: [ :fde, :enable ],                    label: "FDE enabled" },
+    { path: [ :info, :fullname ],                 label: "Full name" },
+    { path: [ :info, :username ],                 label: "User name" },
+    { path: [ :info, :hostname ],                 label: "Host name" },
+    { path: [ :environment, :phone_environment ], label: "Phone environment", mapping: ->(v) do GUI::PhoneEnvironmentConfigurationWindow::ENVIRONMENTS.to_h[v] end },
+  ]
+
+  def configuration_data
+    GUI::SystemConfigurationStepsWindow.instance.configuration_data
+  end
+
+  def configuration_description
+    data = configuration_data
+    DESCRIPTION.map do |description|
+      value = data.dig(*description[:path])
+
+      value = "yes" if value == true
+      value = "no" if value == false
+      if description[:mapping] then
+        value = description[:mapping].call(value)
+      end
+
+      " - #{description[:label]}: #{value.inspect}"
+    end
+      .join("\n")
+  end
+end
