0d06737e49cf2da7b577472994a9dca5fbacc296
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-09-20 17:24:22 -0400

    examples/installer: Add String#indent

--- examples/installer/app/lib/string.rb
+++ examples/installer/app/lib/string.rb
@@ -6,4 +6,11 @@ class String
       "AAAAAAaaaaaaAaAaAaCcCcCcCcCcDdDdDdEEEEeeeeEeEeEeEeEeGgGgGgGgHhHhIIIIiiiiIiIiIiIiIiJjKkkLlLlLlLlLlNnNnNnNnnNnOOOOOOooooooOoOoOoRrRrRrSsSsSsSssTtTtTtUUUUuuuuUuUuUuUuUuUuWwYyyYyYZzZzZz"
     )
   end
+
+  # Indent lines of a string, defaults to two spaces.
+  def indent(chars = "  ")
+    self.split("\n").map do |line|
+      "#{chars}#{line}"
+    end.join("\n")
+  end
 end
