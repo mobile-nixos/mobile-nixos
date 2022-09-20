12724c97a119a6a3fa9bfa52f77c9b733a4c847e
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-09-20 17:24:37 -0400

    examples/installer: Add FileUtils.mkdir_p

---
+++ examples/installer/app/lib/fileutils.rb
@@ -0,0 +1,12 @@
+class FileUtils
+  def self.mkdir_p(*dirs)
+    dirs.each do |dir|
+      # Create all paths in the given hierarchy
+      dir.split("/").reduce("") do |component, acc|
+        dir = [component, acc].join("/")
+        Dir.mkdir(dir) unless Dir.exist?(dir)
+        dir
+      end
+    end
+  end
+end
