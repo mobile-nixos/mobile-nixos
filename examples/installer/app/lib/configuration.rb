1a9b22f06cc32571718a1819702f0257ae3575e8
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-09-21 01:12:48 -0400

    examples/installer: Add plamo config

--- examples/installer/app/lib/configuration.rb
+++ examples/installer/app/lib/configuration.rb
@@ -104,7 +104,24 @@ EOF
 # Plasma Mobile configuration
 #
 
-# TODO
+services.xserver = {
+  enable = true;
+  desktopManager.plasma5.mobile.enable = true;
+  displayManager.defaultSession = "plasma-mobile";
+  displayManager.autoLogin = {
+    enable = true;
+    user = #{username.to_json};
+  };
+  displayManager.lightdm = {
+    enable = true;
+    # Workaround for autologin only working at first launch.
+    # A logout or session crashing will show the login screen otherwise.
+    extraSeatDefaults = ''
+      session-cleanup-script=${pkgs.procps}/bin/pkill -P1 -fx ${pkgs.lightdm}/sbin/lightdm
+    '';
+  };
+  libinput.enable = true;
+};
 EOF
     end
   end
