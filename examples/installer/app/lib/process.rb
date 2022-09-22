6ae92ce4db212145dbedf0ec9dd94453bd658276
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-09-22 17:34:56 -0400

    examples/installer: Prefer Open3 for Process.uid

--- examples/installer/app/lib/process.rb
+++ examples/installer/app/lib/process.rb
@@ -2,6 +2,7 @@ module Process
   def self.uid()
     # TODO: see if we can implement the following with our mruby
     # https://ruby-doc.org/core-2.5.1/Process.html#method-c-uid
-    `id -u`.strip.to_i
+    out, _ = Open3.capture2("id", "-u")
+    out.strip.to_i
   end
 end
