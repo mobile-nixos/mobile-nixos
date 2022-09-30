9433ce445c3fe282ff2ab1a36132367e5cc96d01
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-09-29 20:18:04 -0400

    examples/installer: break out Runner module

--- examples/installer/pkgs/scripted-installer/disk-formatter.rb
+++ examples/installer/pkgs/scripted-installer/disk-formatter.rb
@@ -5,40 +5,6 @@ end
 
 # {{{
 
-module Runner
-  def prettify_command(*args)
-    args = args.dup
-    # Removes the environment hash, if present.
-    args.shift if args.first.is_a?(Hash)
-    if args.length == 1
-      args.first
-    else
-      args.shelljoin
-    end
-  end
-
-  # Runs and pretty-prints a command. Parameters and shelling-out have the same
-  # meaning as with +Kernel#spawn+; one parameter is shelling-out, multiple is
-  # direct +exec+.
-  #
-  # @param args [Array<String>] Command and parameters
-  # @raise [Exception] on exit status 127, commonly used for command not found.
-  # @raise [Exception] on any other exit status.
-  def run(*args)
-    pretty_command = prettify_command(*args)
-    puts(" $ #{pretty_command}")
-    unless system(*args)
-      raise Exception.new("Could not execute `#{pretty_command}`, status nil") if $?.nil?
-      status = $?.exitstatus
-      if status == 127
-        raise Exception.new("Command not found... `#{pretty_command}` (#{status})")
-      else
-        raise Exception.new("Command failed... `#{pretty_command}` (#{status})")
-      end
-    end
-  end
-end
-
 module Helpers
   include Runner
   extend self
