40ce3bbeafb41590ef3e07f451a9571a59f34319
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-10-11 01:21:08 -0400

    examples/phosh: Break out generic config in discrete file
    This file is now public API, and can be imported by end-user
    configurations.

---
+++ examples/phosh/phosh.nix
@@ -0,0 +1,24 @@
+#
+# This file represents safe opinionated defaults for a basic Phosh system.
+#
+# NOTE: this file and any it imports **have** to be safe to import from
+#       an end-user's config.
+#
+{ pkgs, ... }:
+
+{
+  services.xserver.desktopManager.phosh = {
+    enable = true;
+    group = "users";
+  };
+
+  programs.calls.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    chatty              # IM and SMS
+    kgx                 # Terminal
+    megapixels          # Camera
+  ];
+
+  hardware.sensor.iio.enable = true;
+}
