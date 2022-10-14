3d069720f5ef0a3a0a81cf8f08aeb62008a97fe6
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-10-13 23:00:43 -0400

    doc: Make outputs list configurable

--- modules/documentation.nix
+++ modules/documentation.nix
@@ -12,9 +12,19 @@ in
         default = ./system-types + "/${config.mobile.system.type}/device-notes.adoc.erb";
         internal = true;
       };
+      hydraOutputs = mkOption {
+        type = with types; (listOf (listOf str));
+        description = "@device@ will be replaced with the device identifer.";
+        internal = true;
+      };
     };
   };
   config = {
     documentation.nixos.options.splitBuild = false;
+    mobile.documentation.hydraOutputs = lib.mkAfter [
+      # x86_64-linux since we link to the cross-compiled build.
+      # TODO: link to native builds?
+      ["device.@device@.x86_64-linux" "`default` output status"]
+    ];
   };
 }
