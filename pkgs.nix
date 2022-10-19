9794d5c1ce84492968ffa33429281ee3911fcd14
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-10-19 15:57:11 -0400

    pkgs.nix: Update pin
    This syncs to the last completed build for Mobile NixOS, and also the
    first completed build with all requirements for full support for Plasma
    Mobile.

--- pkgs.nix
+++ pkgs.nix
@@ -1,6 +1,6 @@
 let
-  sha256 = "sha256:1s4jizy461hs2l8lbf08qs5dqg54m7k6ii0fblacsdz2rnn2xbjh";
-  rev = "34c5293a71ffdb2fe054eb5288adc1882c1eb0b1";
+  sha256 = "sha256:1nb56lghla61cyyfw27rbsl9hns5898jmrddpa6q7libb4sz5hh4";
+  rev = "32096899af23d49010bd8cf6a91695888d9d9e73";
 in
 builtins.trace "(Using pinned Nixpkgs at ${rev})"
 import (fetchTarball {
