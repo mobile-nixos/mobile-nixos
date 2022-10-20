9b81725b94c4bc0dbf95dcbc10aabeed405c25f7
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-10-20 19:37:54 -0400

    examples/installer: Drop torture test

--- examples/installer/app/main.rb
+++ examples/installer/app/main.rb
@@ -8,11 +8,4 @@ end
 
 GUI::WelcomeWindow.instance.present
 
-if LVGL::Introspection.simulator?
-  LVGUI.main_loop do
-    # Torture test
-    GC.start()
-  end
-else
-  LVGUI.main_loop
-end
+LVGUI.main_loop
