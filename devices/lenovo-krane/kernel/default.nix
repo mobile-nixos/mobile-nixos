dc9728061db6dd96319022a2bdd9b1e7f2353adc
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-11-15 02:11:42 -0500

    lenovo-krane: Fix bluetooth/uart kernel regression

--- devices/lenovo-krane/kernel/default.nix
+++ devices/lenovo-krane/kernel/default.nix
@@ -28,6 +28,12 @@ mobile-nixos.kernel-builder {
       url = "https://github.com/samueldr/linux/commit/7b624a52f799ab01f36989146de43b0ef51f33fd.patch";
       sha256 = "sha256-gop3rD/vroudrTAbf2hWhBqrTAzRXlWEo22bB1ID0QA=";
     })
+    # CHROMIUM: Revert "serial: 8250_mtk: Fix UART_EFR register address"
+    # https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3670640
+    (fetchpatch {
+      url = "https://github.com/torvalds/linux/commit/4cec85ca5a098fca3d49bda9976bccaca16a8876.patch";
+      sha256 = "sha256-V5d1OSJro82LIWrlJ74m5xxF26dtEe7HZmoFgUX/HBc=";
+    })
   ];
 
   isModular = true;
