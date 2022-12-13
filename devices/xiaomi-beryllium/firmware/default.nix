a3808dd05b378b4a155ab409b047b9bfbad3db18
Raymen Scholten <raymenscholten@outlook.com>
2022-12-13 22:17:28 +0000

    Init: xiaomi-beryllium / POCO F1

---
+++ devices/xiaomi-beryllium/firmware/default.nix
@@ -0,0 +1,19 @@
+{ lib
+, fetchFromGitLab
+, runCommand
+}:
+
+let
+  baseFw = fetchFromGitLab {
+    owner = "sdm845-mainline";
+    repo = "firmware-xiaomi-beryllium";
+    rev = "66ec241f43f26e8e8a2c6f18858c92f1a7fb16e4";
+    sha256 = "sha256-7CaXWOpao+vuFA7xknzbLml2hxTlmuzFCEM99aLD2uk=";
+  };
+in runCommand "xiaomi-sdm845-firmware" { inherit baseFw; } ''
+  mkdir -p $out/lib/firmware
+  cp -r $baseFw/lib/firmware/* $out/lib/firmware/
+  chmod +w -R $out
+  # rm -rf $out/lib/firmware/postmarketos
+  # cp -r $baseFw/lib/firmware/postmarketos/* $out/lib/firmware
+''
