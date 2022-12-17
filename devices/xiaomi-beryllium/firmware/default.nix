58a24140e6e2dfaf5811c4eb9afdefd3cea4d6ee
Raymen Scholten <raymenscholten@outlook.com>
2022-12-17 21:06:23 +0000

    Correct hash of firmware

--- devices/xiaomi-beryllium/firmware/default.nix
+++ devices/xiaomi-beryllium/firmware/default.nix
@@ -8,7 +8,7 @@ let
     owner = "sdm845-mainline";
     repo = "firmware-xiaomi-beryllium";
     rev = "66ec241f43f26e8e8a2c6f18858c92f1a7fb16e4";
-    sha256 = "sha256-7CaXWOpao+vuFA7xknzbLml2hxTlmuzFCEM99aLD2uk=";
+    sha256 = "sha256-WhzxM7oooFNbMEQE2OQDFmTTsbvHpuxUtxYJYcPE/7E=";
   };
 in runCommand "xiaomi-sdm845-firmware" { inherit baseFw; } ''
   mkdir -p $out/lib/firmware
