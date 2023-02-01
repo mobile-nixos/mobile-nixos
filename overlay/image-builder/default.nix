86678a40721ea88b20e651f2cd1d20779af2db61
Samuel Dionne-Riel <samuel@dionne-riel.com>
2023-01-31 22:41:05 -0500

    image-builder: Expose pre-baked submodule types
    They can be used like:
    
    ```nix
    options.mobile.generatedFilesystems = mkOption {
      type = types.attrsOf (pkgs.image-builder.types.filesystem-image);
      description = ''
        Filesystem definitions that will be created at build.
      '';
    };
    ```
    
    Take note that the submodule is already evaluated to `config`, so do not
    evaluate again. Relatedly, using a filesystem submodule value within a
    disk image eval requires use of `raw` instead of `filesystem`.

--- overlay/image-builder/default.nix
+++ overlay/image-builder/default.nix
@@ -8,5 +8,16 @@
     inherit pkgs config modules;
   };
 
+  types = {
+    disk-image = lib.types.submodule ({
+      imports = import (./disk-image/module-list.nix);
+      _module.args.pkgs = pkgs;
+    });
+    filesystem-image = lib.types.submodule ({
+      imports = import (./filesystem-image/module-list.nix);
+      _module.args.pkgs = pkgs;
+    });
+  };
+
   helpers = (import ./helpers.nix { inherit lib; }).config.helpers;
 }
