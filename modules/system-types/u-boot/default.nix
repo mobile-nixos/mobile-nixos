a08db9967612aa13656d37eb6697fc34d672d575
Samuel Dionne-Riel <samuel@dionne-riel.com>
2023-02-01 15:43:51 -0500

    system-types/u-boot: Move to new image builder infra

--- modules/system-types/u-boot/default.nix
+++ modules/system-types/u-boot/default.nix
@@ -4,13 +4,15 @@ let
   enabled = config.mobile.system.type == "u-boot";
 
   inherit (config.mobile.outputs) recovery stage-0;
-  inherit (pkgs) buildPackages imageBuilder runCommand;
-  inherit (lib) mkIf mkOption types;
+  inherit (pkgs) buildPackages image-builder runCommand;
+  inherit (lib) mkBefore mkIf mkOption  types;
   cfg = config.mobile.quirks.u-boot;
   inherit (cfg) soc;
   deviceName = config.mobile.device.name;
   kernel = stage-0.mobile.boot.stage-1.kernel.package;
   kernel_file = "${kernel}/${if kernel ? file then kernel.file else pkgs.stdenv.hostPlatform.linux-kernel.target}";
+  inherit (config.mobile.generatedFilesystems) rootfs;
+  boot-partition = config.mobile.generatedFilesystems.boot.output;
 
   # Look-up table to translate from targetPlatform to U-Boot names.
   ubootPlatforms = {
@@ -103,77 +105,6 @@ let
   } ''
     mkimage -C none -A ${ubootPlatforms.${pkgs.stdenv.targetPlatform.system}} -T script -d ${bootcmd} $out
   '';
-
-  # TODO: use generatedFilesystems
-  boot-partition =
-    imageBuilder.fileSystem.makeExt4 {
-      name = "mobile-nixos-boot";
-      partitionLabel = "boot";
-      partitionID = "ED3902B6-920A-4971-BC07-966D4E021683";
-      partitionUUID = "CFB21B5C-A580-DE40-940F-B9644B4466E1";
-      # Let's give us a *bunch* of space to play around.
-      # And let's not forget we have the kernel and stage-1 twice.
-      size = imageBuilder.size.MiB 128;
-      bootable = true;
-      populateCommands = ''
-        mkdir -vp mobile-nixos/{boot,recovery}
-        (
-        cd mobile-nixos/boot
-        cp -v ${stage-0.mobile.outputs.initrd} stage-1
-        cp -v ${kernel_file} kernel
-        cp -vr ${kernel}/dtbs dtbs
-        )
-        (
-        cd mobile-nixos/recovery
-        cp -v ${recovery.mobile.outputs.initrd} stage-1
-        cp -v ${kernel_file} kernel
-        cp -vr ${kernel}/dtbs dtbs
-        )
-        cp -v ${bootscr} ./boot.scr
-      '';
-    }
-  ;
-
-  miscPartition = {
-    # Used as a BCB.
-    name = "misc";
-    partitionLabel = "misc";
-    partitionUUID = "5A7FA69C-9394-8144-A74C-6726048B129D";
-    length = imageBuilder.size.MiB 1;
-    partitionType = "EF32A33B-A409-486C-9141-9FFB711F6266";
-    filename = "/dev/null";
-  };
-
-  persistPartition = imageBuilder.fileSystem.makeExt4 {
-    # To work more like Android-based systems.
-    name = "persist";
-    partitionLabel = "persist";
-    partitionID = "5553F4AD-53E1-2645-94BA-2AFC60C12D38";
-    partitionUUID = "5553F4AD-53E1-2645-94BA-2AFC60C12D39";
-    size = imageBuilder.size.MiB 16;
-    partitionType = "EBC597D0-2053-4B15-8B64-E0AAC75F4DB1";
-  };
-
-  disk-image = imageBuilder.diskImage.makeGPT {
-    name = config.mobile.configurationName;
-    diskID = "01234567";
-
-    partitions = [
-      miscPartition
-      persistPartition
-      boot-partition
-      config.mobile.outputs.generatedFilesystems.rootfs
-    ];
-
-    postProcess = ''
-      (PS4=" $ "; set -x
-      mkdir $out/nix-support
-      cat <<EOF > $out/nix-support/hydra-build-products
-      file disk-image $out/$filename
-      EOF
-      )
-    '';
-  };
 in
 {
   options.mobile = {
@@ -217,11 +148,47 @@ in
   config = lib.mkMerge [
     { mobile.system.types = [ "u-boot" ]; }
     (mkIf enabled {
+      mobile.generatedDiskImages.disk-image = {
+        partitions = mkBefore [
+          {
+            name = "mn-boot";
+            partitionLabel = "boot";
+            partitionUUID = "CFB21B5C-A580-DE40-940F-B9644B4466E1";
+            bootable = true;
+            raw = boot-partition;
+          }
+        ];
+      };
+      mobile.generatedFilesystems.boot = {
+        filesystem = "ext4";
+        # Let's give us a *bunch* of space to play around.
+        # And let's not forget we have the kernel and stage-1 twice.
+        size = pkgs.image-builder.helpers.size.MiB 128;
+
+        ext4.partitionID = "ED3902B6-920A-4971-BC07-966D4E021683";
+        populateCommands = ''
+          mkdir -vp mobile-nixos/{boot,recovery}
+          (
+          cd mobile-nixos/boot
+          cp -v ${stage-0.mobile.outputs.initrd} stage-1
+          cp -v ${kernel_file} kernel
+          cp -vr ${kernel}/dtbs dtbs
+          )
+          (
+          cd mobile-nixos/recovery
+          cp -v ${recovery.mobile.outputs.initrd} stage-1
+          cp -v ${kernel_file} kernel
+          cp -vr ${kernel}/dtbs dtbs
+          )
+          cp -v ${bootscr} ./boot.scr
+        '';
+      };
+
       mobile.outputs = {
         default = config.mobile.outputs.u-boot.disk-image;
         u-boot = {
           inherit boot-partition;
-          disk-image = disk-image;
+          disk-image = config.mobile.generatedDiskImages.disk-image.output;
         };
       };
     })
