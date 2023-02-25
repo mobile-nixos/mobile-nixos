eea40b47466e6c67a277b093a27b55b1716021b4
wentam <wentam42@gmail.com>
2023-02-25 18:03:10 -0500

    pine64-pinephonepro: kernel 6.0.8 -> 6.1.10

--- devices/pine64-pinephonepro/kernel/default.nix
+++ devices/pine64-pinephonepro/kernel/default.nix
@@ -6,14 +6,14 @@
 }:
 
 mobile-nixos.kernel-builder {
-  version = "6.0.8";
+  version = "6.1.10";
   configfile = ./config.aarch64;
 
   src = fetchFromGitLab {
     owner = "pine64-org";
     repo = "linux";
-    rev = "ppp-6.0.8-ppkb";
-    sha256 = "sha256-t7OwU6wRQlU2XCfS1Vd2yWlWzoneFqO0qPa8AZWIAmU=";
+    rev = "ppp-6.1-20230207-2251";
+    sha256 = "sha256-JeG8xlts+Ncmtt5MPu0zkew5qPaq2dDl3Tshs40CSPE=";
   };
 
   patches = [
