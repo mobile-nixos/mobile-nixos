1adb7c14988cb0d9c78794b3d004e6df5ddba556
wentam <wentam42@gmail.com>
2023-02-26 18:44:50 -0500

    pine64-pinephonepro: serial console off by default

--- devices/pine64-pinephonepro/default.nix
+++ devices/pine64-pinephonepro/default.nix
@@ -8,9 +8,10 @@
 
   boot.kernelParams = [
     # Serial console on ttyS2, using the dedicated cable.
-    "console=ttyS2,115200"
-    "earlycon=uart8250,mmio32,0xff1a0000"
-    "earlyprintk"
+    # TODO: option to enable serial console.
+    #"console=ttyS2,115200"
+    #"earlycon=uart8250,mmio32,0xff1a0000"
+    #"earlyprintk"
 
     "quiet"
     "vt.global_cursor_default=0"
