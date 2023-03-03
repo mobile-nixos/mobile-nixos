ea12a06e018cd3f662612e09e191def9bed086d5
Samuel Dionne-Riel <samuel@dionne-riel.com>
2023-03-03 10:32:47 -0500

    boot/init: Add a quirk to wait on USB devices to settle
    This is useful for devices with USB, where input devices may take a
    short while before showing up.
    
    With this, external USB keyboard passphrase input is reliable in the
    limited testing I did.

--- modules/initrd-boot-gui.nix
+++ modules/initrd-boot-gui.nix
@@ -27,6 +27,28 @@ in
       default = true;
       description = "enable splash and boot selection GUI";
     };
+    waitForDevices = {
+      enable = mkOption {
+        type = types.bool;
+        default = false;
+        description = ''
+          Whether to wait a bit for input devices before starting the user interface.
+
+          This is only necessary on "slow" busses where devices may arrive a tad later than expected.
+
+          Generally, only enable when a device needed to input the passphrase is connected via USB.
+        '';
+      };
+      delay = mkOption {
+        type = types.int;
+        default = 2;
+        description = ''
+          Minimum delay spent waiting for input devices to settle.
+
+          The boot GUI will wait until this many seconds elapsed without changes before starting.
+        '';
+      };
+    };
   };
 
   config = mkIf (config.mobile.boot.stage-1.enable) (mkMerge [
@@ -64,6 +86,11 @@ in
       mobile.boot.stage-1.environment = {
         XKB_CONFIG_ROOT = "/etc/X11/xkb";
         XLOCALEDIR = "/etc/X11/locale";
+      };
+      mobile.boot.stage-1.bootConfig = mkIf (cfg.waitForDevices.enable) {
+        quirks = {
+          wait_for_devices_delay = cfg.waitForDevices.delay;
+        };
       };
     })
   ]);
