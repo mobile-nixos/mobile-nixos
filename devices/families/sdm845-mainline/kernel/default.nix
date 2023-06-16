abf4a0cd6179431feb9a4a81a7d477e85acaf847
Lin Yinfeng <lin.yinfeng@outlook.com>
2023-06-16 11:01:43 +0800

    sdm845-mainline/kernel: update to 6.4.0

--- devices/families/sdm845-mainline/kernel/default.nix
+++ devices/families/sdm845-mainline/kernel/default.nix
@@ -5,14 +5,14 @@
 }:
 
 mobile-nixos.kernel-builder rec {
-  version = "6.2.0";
+  version = "6.4.0";
   configfile = ./config.aarch64;
 
   src = fetchFromGitLab {
     owner = "sdm845-mainline";
     repo = "linux";
-    rev = "sdm845-${version}";
-    hash = "sha256-UaCBjFG0TuyZhISO6H6T0oNiBft6rYJGVHZnQgal+HQ=";
+    rev = "sdm845-6.4-r1";
+    hash = "sha256-XUYv8tOk0vsG11w8UtBKizlBZ03cbQ2QRGyZEK0ECGU=";
   };
 
   isModular = false;
