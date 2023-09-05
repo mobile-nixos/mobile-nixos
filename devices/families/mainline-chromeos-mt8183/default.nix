3372aba725e28248a63cf20a663726cc7a0b07e5
Samuel Dionne-Riel <samuel@dionne-riel.com>
2023-09-05 18:44:02 -0400

    families/mainline-chromeos-mt8183: init

---
+++ devices/families/mainline-chromeos-mt8183/default.nix
@@ -0,0 +1,41 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports = [
+    ../mainline-chromeos
+    ./sound.nix
+  ];
+
+  mobile.hardware = {
+    soc = "mediatek-mt8183";
+    ram = lib.mkDefault (1024 * 4);
+  };
+
+  mobile.boot.stage-1 = {
+    kernel.package = pkgs.callPackage ./kernel {};
+  };
+
+  mobile.system.depthcharge.kpart = {
+    dtbs = "${config.mobile.boot.stage-1.kernel.package}/dtbs/mediatek";
+  };
+
+  # Serial console on ttyS0, using a suzyqable or equivalent.
+  mobile.boot.serialConsole = "ttyS0,115200n8";
+
+  systemd.services."serial-getty@ttyS0" = {
+    enable = true;
+    wantedBy = [ "multi-user.target" ];
+  };
+
+  mobile.device.firmware = pkgs.callPackage ./firmware {};
+  mobile.boot.stage-1.firmware = [
+    config.mobile.device.firmware
+  ];
+  mobile.kernel.structuredConfig = [
+    (helpers: with helpers; {
+      # Undeclared dependency needed for some
+      # hid-over-i2c trackpads (e.g. acer-juniper)
+      HID_RMI = yes;
+    })
+  ];
+}
--- devices/lenovo-krane/default.nix
+++ devices/lenovo-krane/default.nix
@@ -2,7 +2,7 @@
 
 {
   imports = [
-    ./sound.nix
+    ../families/mainline-chromeos-mt8183
   ];
 
   mobile.device.name = "lenovo-krane";
@@ -11,38 +11,11 @@
     manufacturer = "Lenovo";
   };
   mobile.device.supportLevel = "supported";
-
   mobile.hardware = {
-    soc = "mediatek-mt8183";
-    ram = 1024 * 4;
     screen = {
       # Panel is portrait CW compared to keyboard attachment.
       width = 1200; height = 1920;
     };
-  };
-
-  mobile.boot.stage-1 = {
-    kernel.package = pkgs.callPackage ./kernel {};
-    kernel.modular = true;
-    kernel.additionalModules = [
-      # Breaks udev if builtin or loaded before udev runs.
-      # Using `additionalModules` means udev will load them as needed.
-      "sbs-battery"
-      "sbs-charger"
-      "sbs-manager"
-    ];
-  };
-
-  mobile.system.depthcharge.kpart = {
-    dtbs = "${config.mobile.boot.stage-1.kernel.package}/dtbs/mediatek";
-  };
-
-  # Serial console on ttyS0, using a suzyqable or equivalent.
-  mobile.boot.serialConsole = "ttyS0,115200n8";
-
-  systemd.services."serial-getty@ttyS0" = {
-    enable = true;
-    wantedBy = [ "multi-user.target" ];
   };
 
   # Ensure orientation match with keyboard.
@@ -50,11 +23,4 @@
     sensor:modalias:platform:*
       ACCEL_MOUNT_MATRIX=0, 1, 0; -1, 0, 0; 0, 0, -1
   '';
-
-  mobile.system.type = "depthcharge";
-
-  mobile.device.firmware = pkgs.callPackage ./firmware {};
-  mobile.boot.stage-1.firmware = [
-    config.mobile.device.firmware
-  ];
 }
