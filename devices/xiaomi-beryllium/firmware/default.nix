66820b0b993e916eb29c85f9190828c55f9ac8a0
Hans Müller <schreibemirhalt@gmail.com>
2023-12-28 16:05:25 +0100

    Pull newer beryllium firmware; 20th of May 2023
    Commit message: Add adsp adbc files

--- devices/xiaomi-beryllium/firmware/default.nix
+++ devices/xiaomi-beryllium/firmware/default.nix
@@ -7,8 +7,8 @@ let
   baseFw = fetchFromGitLab {
     owner = "sdm845-mainline";
     repo = "firmware-xiaomi-beryllium";
-    rev = "66ec241f43f26e8e8a2c6f18858c92f1a7fb16e4";
-    sha256 = "sha256-WhzxM7oooFNbMEQE2OQDFmTTsbvHpuxUtxYJYcPE/7E=";
+    rev = "9ce691fb2e629476a33e037bdcd039cd7e8c5a6c";
+    sha256 = "sha256-R/x7LxDyPU3s30y2PJNw72vo7wAA3Di8Iy61syffEKE=";
   };
 in runCommand "xiaomi-sdm845-firmware" { inherit baseFw; } ''
   mkdir -p $out/lib/firmware
