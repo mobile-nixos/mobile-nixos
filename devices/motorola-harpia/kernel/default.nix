45954a8116749115860328423f84149bf139723b
Daniel Barlow <dan@telent.net>
2024-02-14 23:43:05 +0000

    harpia kernel patch to enable simplefb and pstore

--- devices/motorola-harpia/kernel/default.nix
+++ devices/motorola-harpia/kernel/default.nix
@@ -3,16 +3,24 @@
 , ...
 }:
 
-mobile-nixos.kernel-builder {
-  version = "6.7.0-rc4";
-  configfile = ./config.aarch64;
-
+let
   src = fetchFromGitHub {
     owner = "msm8916-mainline";
     repo = "linux";
     rev = "6cc2342c6a7227a05ca94c836df42d5ba273eef2";  #  branch msm8916/6.7-rc4
     sha256 = "sha256-zyAfQwiGx6vCdfRpnZl4qTqAM5TkIda7E0APnDiSF3E=";
   };
+in
+mobile-nixos.kernel-builder {
+  version = "6.7.0-rc4";
+  configfile = ./config.aarch64;
 
+  inherit src;
+  # bq24296 is charger-related
+  # max170xx_battery
+
+  patches = [
+    ./pstore-and-simplefb-harpia.patch
+  ];
   isModular = true;
 }
