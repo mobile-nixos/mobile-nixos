163a9e3252b011d1746d41c1cb16d6338d4b5058
Luflosi <luflosi@luflosi.de>
2024-05-13 10:19:30 +0200

    pine64-pinephone: kernel 6.8.8 -> 6.9.0

--- devices/pine64-pinephone/kernel/default.nix
+++ devices/pine64-pinephone/kernel/default.nix
@@ -6,14 +6,14 @@
 }:
 
 mobile-nixos.kernel-builder {
-  version = "6.8.8";
+  version = "6.9.0";
   configfile = ./config.aarch64;
   src = fetchFromGitea {
     domain = "codeberg.org";
     owner = "megi";
     repo = "linux";
-    rev = "orange-pi-6.8-20240429-1915";
-    hash = "sha256-G+hT1uqW4F2ajQcFya6I9RD/xZtEhDXpK9C4ESiyT8U=";
+    rev = "orange-pi-6.9-20240512-2332";
+    hash = "sha256-6hWft1+EGdFZo4My1lsr2PkJBgOcoKrLYSgJSGrM/Z8=";
   };
   patches = [
     ./0001-dts-pinephone-Setup-default-on-and-panic-LEDs.patch
