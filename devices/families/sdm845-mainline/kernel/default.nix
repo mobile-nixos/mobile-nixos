f4d512dc87bc13139e063e121e1c5d02260c87fd
matthewcroughan <matt@croughan.sh>
2024-09-23 14:53:16 +0000

    sdm845-mainline: kernel 6.4.0 -> 6.11

--- devices/families/sdm845-mainline/kernel/default.nix
+++ devices/families/sdm845-mainline/kernel/default.nix
@@ -1,27 +1,18 @@
 { mobile-nixos
 , fetchFromGitLab
-, fetchpatch
 , ...
 }:
 
 mobile-nixos.kernel-builder {
-  version = "6.4.0";
+  version = "6.11.0";
   configfile = ./config.aarch64;
 
   src = fetchFromGitLab {
     owner = "sdm845-mainline";
     repo = "linux";
-    rev = "sdm845-6.4-r1";
-    hash = "sha256-XUYv8tOk0vsG11w8UtBKizlBZ03cbQ2QRGyZEK0ECGU=";
+    rev = "sdm845-6.11.0";
+    hash = "sha256-Mf6lCozshFMSFHiy7AjS4dQ/jVWn0hXKFLcSHxwAwCc=";
   };
-
-  patches = [
-    # ASoC: codecs: tas2559: Fix build
-    (fetchpatch {
-      url = "https://github.com/samueldr/linux/commit/d1b59edd94153ac153043fb038ccc4e6c1384009.patch";
-      sha256 = "sha256-zu1m+WNHPoXv3VnbW16R9SwKQzMYnwYEUdp35kUSKoE=";
-    })
-  ];
 
   isModular = false;
   isCompressed = "gz";
