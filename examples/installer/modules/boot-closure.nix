a465ac0decae7d9277b8db85463dd79b669cac21
Samuel Dionne-Riel <samuel@dionne-riel.com>
2024-12-19 00:07:33 -0500

    examples/installer: Also provide extraUtils for stage-1

--- examples/installer/modules/boot-closure.nix
+++ examples/installer/modules/boot-closure.nix
@@ -24,6 +24,11 @@ in
         '') (
           [
           ]
+          ++ (
+            builtins.map
+            (p: { path = if p ? package then p.package else p; })
+            config.mobile.boot.stage-1.extraUtils
+          )
           ++ optional (system.type == "depthcharge") { path = config.mobile.outputs.depthcharge.kpart; }
           ++ optional (system.type == "u-boot") { path = config.mobile.outputs.u-boot.boot-partition; name = "installer.stage-1.img"; }
         )
