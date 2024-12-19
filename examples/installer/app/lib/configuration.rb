59d45e8818d2d3b7db9116481d0bcb93e7172f21
Samuel Dionne-Riel <samuel@dionne-riel.com>
2024-12-18 23:48:50 -0500

    examples/installer: Add "bogus" qemu-uefi system
    The configuration will be valid when paired with this imperative change:
    
    ```
    ln -s ../support/additional-devices/uefi-x86_64-with-kernel devices/qemu-uefi
    ```

--- examples/installer/app/lib/configuration.rb
+++ examples/installer/app/lib/configuration.rb
@@ -51,11 +51,11 @@ module Configuration
       # Let's try and detect the device with its DMI info
       if File.exist?("/sys/class/dmi/id/product_name")
         product_name = File.read("/sys/class/dmi/id/product_name")
-        # Bogus example of an UEFI system detection
-        #case product_name
-        #when "Jupiter"
-        #  return "valve-jupiter"
-        #end
+        chassis_vendor = File.read("/sys/class/dmi/id/chassis_vendor")
+        case chassis_vendor
+        when /^QEMU$/
+          return "qemu-uefi"
+        end
       end
 
       # Oh, still nothing specific? Let's hope it's just generic UEFI!
@@ -105,6 +105,9 @@ module Configuration
         when "acer-lazor", "lenovo-wormdingler"
           # Qualcomm 7c eMMC
           File.join("/dev/disk/by-path", "platform-7c4000.mmc")
+        when "qemu-uefi"
+          # QEMU using e.g. `./result -drive "file=target.img"` for a second drive.
+          File.join("/dev/disk/by-id/", "ata-QEMU_HARDDISK_QM00002")
         end
 
       raise "Unknown target disk" unless path
