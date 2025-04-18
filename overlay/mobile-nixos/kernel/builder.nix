4881adc7f5d8db77b485b3a24ac792d7e2b35a03
Daniel Barlow <dan@telent.net>
2025-04-18 01:09:16 +0100

    kernel needs python3 for msm panel generator

--- overlay/mobile-nixos/kernel/builder.nix
+++ overlay/mobile-nixos/kernel/builder.nix
@@ -25,6 +25,7 @@
 , writeShellScriptBin
 
 , perl
+, python3
 , bc
 , net-tools
 , openssl
@@ -216,7 +217,7 @@ stdenv.mkDerivation (inputArgs // {
   updateConfigFromStructuredConfig = !__mobile-nixos-useStrictKernelConfig;
 
   depsBuildBuild = [ buildPackages.stdenv.cc ];
-  nativeBuildInputs = [ perl bc net-tools openssl rsync gmp libmpc mpfr ]
+  nativeBuildInputs = [ perl python3  bc net-tools openssl rsync gmp libmpc mpfr ]
     ++ optional (platform.linux-kernel.target == "uImage") buildPackages.ubootTools
     ++ optional (lib.versionAtLeast version "4.14" && lib.versionOlder version "5.8") libelf
     ++ optional (lib.versionAtLeast version "4.15") util-linux
