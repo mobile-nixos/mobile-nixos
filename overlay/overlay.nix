6f849df4dcd7fdd6e12266aa330da010ea49a69b
Kurt Morris <kurt.morris@me.com>
2025-04-18 23:27:38 -0700

    merge pr:  unbound: fix cross-compiling issue with yacc #789

--- overlay/overlay.nix
+++ overlay/overlay.nix
@@ -147,4 +147,27 @@ in
     };
 
     image-builder = callPackage ./image-builder {};
- }
+ } // (super.lib.optionalAttrs (super.stdenv.hostPlatform != super.stdenv.buildPlatform) {
+     #
+     # Nixpkgs cross-compilation workarounds
+     # -------------------------------------
+     #
+
+     ruby_3_3 = super.ruby_3_3.overrideAttrs({
+       # https://github.com/samueldr/nixpkgs/compare/88195a94f390381c6afcdaa933c2f6ff93959cb4...fix/ruby-yjit-cross
+       NIX_RUSTFLAGS = "--target ${super.stdenv.hostPlatform.rust.rustcTargetSpec}";
+     });
+     ruby_3_4 = super.ruby_3_4.overrideAttrs({
+       # https://github.com/samueldr/nixpkgs/compare/88195a94f390381c6afcdaa933c2f6ff93959cb4...fix/ruby-yjit-cross
+       NIX_RUSTFLAGS = "--target ${super.stdenv.hostPlatform.rust.rustcTargetSpec}";
+     });
+
+     unbound = super.unbound.overrideAttrs (old: {
+       # https://github.com/NixOS/nixpkgs/commit/a36d820bab68e43bf841e45ed168dc6f38bdf83c
+       nativeBuildInputs = old.nativeBuildInputs ++ [
+         super.bison
+         super.flex
+         super.pkg-config
+       ];
+     });
+  })
