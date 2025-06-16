8288724a45dce0417973cb02f9ac77d3684ec35e
tajo48 <tajo48@proton.me>
2025-06-16 13:02:14 +0200

    retroid-pocket-5: Fix device configuration and screen parameters

--- devices/retroid-pocket-5/default.nix
+++ devices/retroid-pocket-5/default.nix
@@ -13,17 +13,38 @@
   mobile.device.name = "retroid-pocket-5";
   mobile.device.identity = {
     name = "Retroid Pocket 5";
-    manufacturer = "Retroid";
+    manufacturer = "Moorechip";
   };
   mobile.device.supportLevel = "best-effort";
 
   mobile.hardware = {
     ram = 1024 * 8;
     screen = {
-      width = 1080;
-      height = 2280;
+      width = 1920;
+      height = 1080;
     };
   };
+
+  boot.kernelParams = [
+    "clk_ignore_unused"
+    "pd_ignore_unused"
+    "arm64.nopauth"
+    "efi=noruntime"
+    "console=ttyMSM0,115200n8"
+    "fbcon=rotate:3"
+  ];
+
+  boot.kernelModules = [ "panel-ddic-ch13726a" ];
+  boot.initrd.availableKernelModules = [
+    "panel-ddic-ch13726a"
+    "qcom_spmi_haptics"
+    "qcom_glink_smem"
+    "qcom_smd"
+    "qcom_common"
+    "rpmhpd"
+    "qcom_rpmh"
+    "cmd-db"
+  ];
 
   mobile.device.firmware = pkgs.callPackage ./firmware { };
 
