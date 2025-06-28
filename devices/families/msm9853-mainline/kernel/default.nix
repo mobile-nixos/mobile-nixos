8c251da4b588bdb01fddff9e4414d5f2a9665b2e
Devon Hockley <devon@hockley.ca>
2025-06-28 16:01:06 -0600

    test

---
+++ devices/families/msm9853-mainline/kernel/default.nix
@@ -0,0 +1,28 @@
+{ mobile-nixos
+, fetchFromGitHub
+, fetchpatch
+, ...
+}:
+
+mobile-nixos.kernel-builder {
+  version = "6.15.0";
+  configfile = ./config.aarch64;
+
+  src = fetchFromGitHub {
+    owner = "msm8953-mainline";
+    repo = "linux";
+    rev = "v6.15.0-r0";
+    hash = "sha256-0KUOEQyrwKUzS+lVxPIaz2rELLcC6VPIG8B9+WkngLc=";
+  };
+
+#   patches = [
+#     # ASoC: codecs: tas2559: Fix build
+#     (fetchpatch {
+#       url = "https://github.com/samueldr/linux/commit/d1b59edd94153ac153043fb038ccc4e6c1384009.patch";
+#       sha256 = "sha256-zu1m+WNHPoXv3VnbW16R9SwKQzMYnwYEUdp35kUSKoE=";
+#     })
+#   ];
+
+  isModular = false;
+  isCompressed = "gz";
+}
---
+++ devices/lenovo-cd-18781y/default.nix
@@ -0,0 +1,26 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports = [
+    ../families/msm9853-mainline
+  ];
+
+  mobile.device.name = "lenovo-cd-18781y";
+  mobile.device.identity = {
+    name = "ThinkSmart View";
+    manufacturer = "Lenovo";
+  };
+  mobile.device.supportLevel = "supported";
+  mobile.hardware = {
+    screen = {
+      # Panel is portrait CW compared to keyboard attachment.
+      width = 800; height = 1280;
+    };
+  };
+#
+#   # Ensure orientation match with keyboard.
+#   services.udev.extraHwdb = lib.mkBefore ''
+#     sensor:modalias:platform:*
+#       ACCEL_MOUNT_MATRIX=0, 1, 0; -1, 0, 0; 0, 0, -1
+#   '';
+}
