427c25cc105fefd098886cc91d4725f083da5922
benaryorg <binary@benary.org>
2025-08-30 18:38:53 +0000

    Revert "gui: Don't override libraries for stage-2"
    See https://redirect.github.com/mobile-nixos/mobile-nixos/pull/826#issuecomment-3184696599
    
    This reverts commit e901061aa62943daf3173796a737e91a7c857472.

--- boot/script-loader/mruby-lvgui-native/lvgui.nix
+++ boot/script-loader/mruby-lvgui-native/lvgui.nix
@@ -7,8 +7,6 @@
 , SDL2
 , libdrm
 , libevdev
-, libinput
-, libxkbcommon
 , withSimulator ? false
 }:
 
@@ -17,6 +15,76 @@ let
   simulatorDeps = [
     SDL2
   ];
+
+  # Minified libinput, both for size and cross-compilation.
+  libinput = (pkgs.libinput.override({
+    # libwacom doesn't cross-compile at the moment
+    libwacom = null;
+
+    documentationSupport = false;
+    doxygen = null;
+    graphviz = null;
+
+    eventGUISupport = false;
+    cairo = null;
+    glib = null;
+    gtk3 = null;
+
+    testsSupport = false;
+    check = null;
+    valgrind = null;
+    python3 = null;
+  })).overrideAttrs(old: {
+    buildInputs = with pkgs; [
+      libevdev
+      mtdev
+    ];
+    nativeBuildInputs = old.nativeBuildInputs ++ [
+      pkgs.buildPackages.udev
+    ];
+    mesonFlags = old.mesonFlags ++ [
+      "-Dlibwacom=false"
+    ];
+  });
+
+  libxkbcommon = pkgs.callPackage (
+    { stdenv
+    , libxkbcommon
+    , meson
+    , ninja
+    , pkg-config
+    , bison
+    }:
+
+    libxkbcommon.overrideAttrs({...}: {
+      nativeBuildInputs = [ meson ninja pkg-config bison ];
+      buildInputs = [ ];
+
+      mesonFlags = [
+        "-Denable-wayland=false"
+        "-Denable-x11=false"
+        "-Denable-docs=false"
+        "-Denable-xkbregistry=false"
+
+        # This is because we're forcing uses of this build
+        # to define config and locale root; for stage-1 use.
+        # In stage-2, use the regular xkbcommon lib.
+        "-Dxkb-config-root=/NEEDS/OVERRIDE/etc/X11/xkb"
+        "-Dx-locale-root=/NEEDS/OVERRIDE/share/X11/locale"
+      ];
+
+      outputs = [ "out" "dev" ];
+
+      # Ensures we don't get any stray dependencies.
+      allowedReferences = [
+        "out"
+        "dev"
+        stdenv.cc.libc_lib
+      ];
+    })
+
+  ) {};
+
 in
   stdenv.mkDerivation {
     pname = "lvgui";
