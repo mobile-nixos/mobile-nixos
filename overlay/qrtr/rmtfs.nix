3be2b8a92991a8bd27e93cc4167e34c272cba6aa
Petr Hodina <petr.hodina@protonmail.com>
2026-01-27 15:29:14 +0000

    overlay: qrtr: Update rtmfs
    Signed-off-by: Petr Hodina <petr.hodina@protonmail.com>

--- overlay/qrtr/rmtfs.nix
+++ overlay/qrtr/rmtfs.nix
@@ -1,24 +1,35 @@
-{ stdenv, lib, fetchFromGitHub, udev, qrtr, qmic }:
-
-stdenv.mkDerivation {
+{
+  stdenv,
+  lib,
+  fetchFromGitHub,
+  udev,
+  qrtr,
+  qmic,
+}:
+stdenv.mkDerivation rec {
   pname = "rmtfs";
-  version = "unstable-2022-07-18";
+  version = "1.2";
 
-  buildInputs = [ udev qrtr qmic ];
+  buildInputs = [
+    udev
+    qrtr
+    qmic
+  ];
 
   src = fetchFromGitHub {
-    owner = "andersson";
+    owner = "linux-msm";
     repo = "rmtfs";
-    rev = "695d0668ffa6e2a4bf6e676f3c58a444a5d67690";
-    hash = "sha256-00KOjdkwcAER261lleSl7OVDEAEbDyW9MWxDd0GI8KA=";
+    tag = "v${version}";
+    hash = "sha256-fjyiSl9a98goqd/tQSylcSZFM5NAK9Kaa+5M28wa78U=";
   };
 
   installFlags = [ "prefix=$(out)" ];
 
   meta = with lib; {
     description = "Qualcomm Remote Filesystem Service";
-    homepage = "https://github.com/andersson/rmtfs";
+    homepage = "https://github.com/linux-msm/rmtfs";
     license = licenses.bsd3;
+    maintainers = [ ];
     platforms = platforms.aarch64;
   };
 }
