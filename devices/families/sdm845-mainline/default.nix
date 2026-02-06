c1a74fe05995e2836478c8aac81212638faa07b1
Petr Hodina <petr.hodina@protonmail.com>
2026-02-06 12:37:02 +0000

    sdm845-mainline: replace wildcard rm with find when removing modem.mbn
    Signed-off-by: Petr Hodina <petr.hodina@protonmail.com>

--- devices/families/sdm845-mainline/default.nix
+++ devices/families/sdm845-mainline/default.nix
@@ -26,7 +26,7 @@
       cp -vrf ${config.mobile.device.firmware} $out
       chmod -R +w $out
       # Big file, fills and breaks stage-1
-      rm -v $out/lib/firmware/qcom/sdm845/*/modem.mbn
+      find $out/lib/firmware/qcom/sdm845 -name "modem.mbn" -delete -print
 
       # Copy extra a630 firmware from linux-firmware
       cp -vf ${pkgs.linux-firmware}/lib/firmware/qcom/{a630_sqe.fw,a630_gmu.bin} $out/lib/firmware/qcom
